c40d11a24a8985a726a5c69d4cc57149eb0179ea
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:43:24 +0400

    Commiting all

---
+++ src/bundle/ishield/dialog/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by _IsUser.RC
+//
+#define DLG_TEMPLATE                    13029
+#define DLG_TEMPLATE2                   13030
+#define GENSYM_REGISTER_USER            13031
+#define GENSYM_SETUP_TYPE               32009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
