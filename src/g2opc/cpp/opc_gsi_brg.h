25272bfad88659b709d5b2c11ad616fe5dcd3bf2
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:07:36 +0400

    Commiting all

---
+++ src/g2opc/cpp/opc_gsi_brg.h
@@ -0,0 +1,11 @@
+#ifndef _GSI_BRG_H_
+#define _GSI_BRG_H_
+
+/* Prototypes for authorization functions: */
+/* --------------------------------------- */
+short int stage1(char *product, char *version) ;
+short int stage2() ;
+short int periodic() ;
+
+#endif
+
