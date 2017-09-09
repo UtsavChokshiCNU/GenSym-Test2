b3d18b6f47b3fc4fd65140ba773c6e80d929e5a5
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:05:35 +0400

    Commiting all

---
+++ src/g2i/sites/e-SCOR/doc/java-help-4x/Tutorials/html/e-SCOR_Tutorials.hs
@@ -0,0 +1,32 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<helpset version="1.0">
+  <title>e-SCOR_Tutorials</title>
+  <maps>
+    <homeID>preface_htm_25184</homeID>
+    <mapref location="e-SCOR_Tutorials.jhm" />
+  </maps>
+  <view>
+    <name>TOC</name>
+    <label>e-SCOR_Tutorials</label>
+    <type>javax.help.TOCView</type>
+    <data>booktoc.xml</data>
+  </view>
+  <view>
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>bookix.xml</data>
+  </view>
+  <view>
+    <name>Search</name>
+    <label>Search</label>
+    <type>javax.help.SearchView</type>
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">
+      JavaHelpSearch
+    </data>
+  </view>
+</helpset>
