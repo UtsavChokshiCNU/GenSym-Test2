61669b88a810f88c2914a73e07a0154376b8582d
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:09 +0400

    Commiting all

---
+++ src/g2db/env/syb1111-sparcsol.bat
@@ -0,0 +1,3 @@
+setenv SYBASE /gensym/spare/bridges/sparcsol/dbs/sybase/1111
+set olp = $LD_LIBRARY_PATH
+setenv LD_LIBRARY_PATH {$LD_LIBRARY_PATH}:/gensym/spare/bridges/sparcsol/dbs/sybase/1111/lib
