d51343dadf9dc7da3ceb9cf8a8b6c3ea8f7bade3
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:58:41 +0400

    Commiting all

---
+++ src/ext/c2/Interop/dllLink.c
@@ -0,0 +1,11 @@
+/* Linux skeleton code for DLL interoperability in KB language
+ * Subsystem: EXT.Subsystems.Interop
+ * Tags:
+ * 
+ * Copyright (C) 1986-2017 Gensym Corporation.  All Rights Reserved.
+ */
+#include "g2ss/Interop/dllLink.h"
+
+int XANALYS_API g2ext_load_dynamic_library(void *path) {
+	return 0;
+}
