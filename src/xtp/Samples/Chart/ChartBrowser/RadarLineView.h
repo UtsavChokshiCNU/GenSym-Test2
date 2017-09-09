f626f03eb492f801874f157a733fbe9fe718b63d
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:47:50 +0400

    Commiting all

---
+++ src/xtp/Samples/Chart/ChartBrowser/RadarLineView.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "BaseView.h"
+
+
+// CRadarLineView form view
+
+class CRadarLineView : public CBaseView
+{
+	DECLARE_DYNCREATE(CRadarLineView)
+
+protected:
+	CRadarLineView();           // protected constructor used by dynamic creation
+	virtual ~CRadarLineView();
+
+public:
+	enum { IDD = IDD_VIEW_RADARLINE };
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+#ifndef _WIN32_WCE
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+#endif
+
+	BOOL m_bShowLabels;
+	int m_nMarkerType;
+	int m_nMarkerSize;
+
+	BOOL m_bShowAxisX;
+	BOOL m_bShowAxisY;
+
+	BOOL m_bInterlaced;
+
+	void CreateChart();
+
+	void OnInitialUpdate();
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+
+	afx_msg void OnCheckShowLabels();
+	afx_msg void OnCbnSelchangeComboMarkerSize();
+	afx_msg void OnCbnSelchangeComboMarkerType();
+	afx_msg void OnCheckShowAxisX();
+	afx_msg void OnCheckShowAxisY();
+	afx_msg void OnCheckInterlaced();
+public:
+};
+
+
