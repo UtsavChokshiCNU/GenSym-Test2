efa34e029a4f2c3ee5e85b62472d6871e0e2a76b
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:17 +0400

    Commiting all

---
+++ src/g2db/java/G2JDBC/src/type/LogFilteringLevel.java
@@ -0,0 +1,20 @@
+package type;
+
+/**
+ * Stores log filtering constants, used in the g2-database.kb
+ * @author SoftServe
+ *
+ */
+public class LogFilteringLevel {
+	
+	public static final String ALL = "ALL";
+	public static final String FATAL = "FATAL";
+	public static final String ERROR = "ERROR";
+	public static final String WARN1 = "WARN";
+	public static final String WARN2 = "WARNING";
+	public static final String INFO = "INFO";
+	
+	private LogFilteringLevel() {
+		
+	}
+}
