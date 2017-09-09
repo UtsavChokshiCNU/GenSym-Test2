3811aff519204a437ed8eb293b36f2db1c9261c5
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:05:17 +0400

    Commiting all

---
+++ src/g2i/java/test/com/gensym/SocketManager/SocketManagerTest.java
@@ -0,0 +1,12 @@
+package com.gensym.SocketManager;
+
+import com.gensym.SocketManager.G2JavaLog;
+
+import junit.framework.TestCase;
+ 
+public class SocketManagerTest extends TestCase {
+	public void testG2JavaLog(){
+		G2JavaLog g2javalog = new G2JavaLog();
+	}
+    
+}
