09dde3380bc81c04496657295c4aae31802dcb31
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:42:25 +0400

    Commiting all

---
+++ src/activex/tw2-client/CProxy_G2WorkspacePanelSource.h
@@ -0,0 +1,109 @@
+#include "AXClientConnectionPointImpl.h"
+
+//////////////////////////////////////////////////////////////////////////////
+// CProxy_G2WorkspacePanelSource
+template <class T>
+class CProxy_G2WorkspacePanelSource : public IAXClientConnectionPointImpl<T, &DIID__G2WorkspacePanelSource, CComDynamicUnkArray>
+{
+public:
+//methods:
+//_G2WorkspacePanelSource : IDispatch
+public:
+	void Fire_SelectionChanged()
+	{
+		T* pT = (T*)this;
+		pT->Lock();
+		IUnknown** pp = m_vec.begin();
+		while (pp < m_vec.end())
+		{
+			if (*pp != NULL)
+			{
+				DISPPARAMS disp = { NULL, NULL, 0, 0 };
+				IDispatch* pDispatch = reinterpret_cast<IDispatch*>(*pp);
+				pDispatch->Invoke(0x0, IID_NULL, LOCALE_USER_DEFAULT, DISPATCH_METHOD, &disp, NULL, NULL, NULL);
+			}
+			pp++;
+		}
+		pT->Unlock();
+	}
+	void Fire_KeyUp(
+		short * KeyCode,
+		short shift)
+	{
+		VARIANTARG* pvars = new VARIANTARG[2];
+		for (int i = 0; i < 2; i++)
+			VariantInit(&pvars[i]);
+		T* pT = (T*)this;
+		pT->Lock();
+		IUnknown** pp = m_vec.begin();
+		while (pp < m_vec.end())
+		{
+			if (*pp != NULL)
+			{
+				pvars[1].vt = VT_I2 | VT_BYREF;
+				pvars[1].byref= KeyCode;
+				pvars[0].vt = VT_I2;
+				pvars[0].iVal= shift;
+				DISPPARAMS disp = { pvars, NULL, 2, 0 };
+				IDispatch* pDispatch = reinterpret_cast<IDispatch*>(*pp);
+				pDispatch->Invoke(DISPID_KEYUP, IID_NULL, LOCALE_USER_DEFAULT, DISPATCH_METHOD, &disp, NULL, NULL, NULL);
+			}
+			pp++;
+		}
+		pT->Unlock();
+		delete[] pvars;
+	}
+	void Fire_KeyPress(
+		short * KeyAscii)
+	{
+		VARIANTARG* pvars = new VARIANTARG[1];
+		for (int i = 0; i < 1; i++)
+			VariantInit(&pvars[i]);
+		T* pT = (T*)this;
+		pT->Lock();
+		IUnknown** pp = m_vec.begin();
+		while (pp < m_vec.end())
+		{
+			if (*pp != NULL)
+			{
+				pvars[0].vt = VT_I2 | VT_BYREF;
+				pvars[0].byref= KeyAscii;
+				DISPPARAMS disp = { pvars, NULL, 1, 0 };
+				IDispatch* pDispatch = reinterpret_cast<IDispatch*>(*pp);
+				pDispatch->Invoke(DISPID_KEYPRESS, IID_NULL, LOCALE_USER_DEFAULT, DISPATCH_METHOD, &disp, NULL, NULL, NULL);
+			}
+			pp++;
+		}
+		pT->Unlock();
+		delete[] pvars;
+	}
+	void Fire_KeyDown(
+		short * KeyCode,
+		short shift)
+	{
+		VARIANTARG* pvars = new VARIANTARG[2];
+		for (int i = 0; i < 2; i++)
+			VariantInit(&pvars[i]);
+		T* pT = (T*)this;
+		pT->Lock();
+		IUnknown** pp = m_vec.begin();
+		while (pp < m_vec.end())
+		{
+			if (*pp != NULL)
+			{
+				pvars[1].vt = VT_I2 | VT_BYREF;
+				pvars[1].byref= KeyCode;
+				pvars[0].vt = VT_I2;
+				pvars[0].iVal= shift;
+				DISPPARAMS disp = { pvars, NULL, 2, 0 };
+				IDispatch* pDispatch = reinterpret_cast<IDispatch*>(*pp);
+				pDispatch->Invoke(DISPID_KEYDOWN, IID_NULL, LOCALE_USER_DEFAULT, DISPATCH_METHOD, &disp, NULL, NULL, NULL);
+			}
+			pp++;
+		}
+		pT->Unlock();
+		delete[] pvars;
+	}
+
+};
+
