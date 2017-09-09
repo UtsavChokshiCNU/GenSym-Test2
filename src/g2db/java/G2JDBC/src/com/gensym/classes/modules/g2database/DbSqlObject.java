c99bf0906b47079db1e2b55d866a9dff811d3824
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:15 +0400

    Commiting all

---
+++ src/g2db/java/G2JDBC/src/com/gensym/classes/modules/g2database/DbSqlObject.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (C) 1986-2017 Gensym Corporation.
+ *  All Rights Reserved.
+ *
+ *      DbSqlObject.java
+ *
+ *   Description: Generated Interface file. Do not edit!
+ *
+ *        Author: Gensym Corp.
+ *
+ *       Version: 5.1 Rev. 1
+ *
+ *          Date: Thu Jun 18 17:47:12 EEST 2009
+ *
+ */
+
+
+package com.gensym.classes.modules.g2database;
+
+import com.gensym.classes.*;
+import com.gensym.util.Structure;
+import com.gensym.util.Sequence;
+import com.gensym.util.Symbol;
+import com.gensym.util.symbol.SystemAttributeSymbols;
+import com.gensym.jgi.*;
+import com.gensym.classes.Object;
+
+public interface DbSqlObject extends com.gensym.classes.modules.g2database.DbConfigObject {
+
+  public static final Symbol DB_SQL_OBJECT_ = Symbol.intern ("DB-SQL-OBJECT");
+  static final Symbol g2ClassName = DB_SQL_OBJECT_;
+  static final Symbol[] classInheritancePath = new Symbol[] {DB_SQL_OBJECT_, DB_CONFIG_OBJECT_, GSI_DATA_SERVICE_, SYMBOLIC_VARIABLE_, G2_VARIABLE_, VARIABLE_, VARIABLE_OR_PARAMETER_, OBJECT_, ITEM_, ENTITY_, BLOCK_, KB_FRAME_, SYSTEM_ITEM_, ROOT_};
+
+
+  static final Symbol[] staticAttributes = new Symbol[] {};
+
+
+}
