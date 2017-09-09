2fe29f3eb0eb9aa411c2c983183ea3af7d685177
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:05:09 +0400

    Commiting all

---
+++ src/g2i/java/com/gensym/SocketManager/G2Version.java
@@ -0,0 +1,10 @@
+package com.gensym.SocketManager;
+
+public class G2Version {
+
+  public static final String VERSION_INFORMATION = "Version 4.3 Alpha Rev. 0";
+  public static final String BUILD_INFORMATION = "4030000";
+  public static final String BUILD_DATE = "30 Oct 2013  9:06:34 p.m.";
+  public static final String COPYRIGHT_INFORMATION = "Gensym Corporation 2002-2013";
+ 
+}
