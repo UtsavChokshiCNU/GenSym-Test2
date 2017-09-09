7ffd3c261e97eb284e9a028c537aea341258b7f5
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:49:20 +0400

    Commiting all

---
+++ src/xtp/Samples/ReportControl/TrackControl/Resource.h
@@ -0,0 +1,70 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by TrackControl.rc
+//
+#define IDR_MANIFEST                    1
+#define IDM_ABOUTBOX                    0x0010
+#define IDS_ABOUTBOX                    101
+#define IDD_REPORTDIALOG_DIALOG         102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU_POPUP                  129
+#define IDD_DIALOG_PROPERTIES           130
+#define IDD_DIALOG_MARKER               131
+#define IDB_BMREPORT                    200
+#define IDC_REPORTCTRL                  1002
+#define IDC_SLIDER1                     1003
+#define IDC_SLIDER                      1003
+#define IDC_SCROLLBAR2                  1005
+#define IDC_SCROLLBAR                   1006
+#define IDC_EDIT_MIN                    1007
+#define IDC_EDIT_MAX                    1008
+#define IDC_EDIT_CAPTION                1008
+#define IDC_EDIT_POSITION               1009
+#define IDS_GROUP_DATE_OLDER            2000
+#define IDS_GROUP_DATE_THISYEAR         2001
+#define IDS_GROUP_DATE_THISMONTH        2002
+#define IDS_GROUP_DATE_TODAY            2003
+#define IDS_GROUP_CHECKED_TRUE          2004
+#define IDS_GROUP_CHECKED_FALSE         2005
+#define IDS_GROUP_IMPOPRTANCE_HIGH      2006
+#define IDS_GROUP_IMPOPRTANCE_NORMAL    2007
+#define IDS_GROUP_IMPOPRTANCE_LOW       2008
+#define IDS_GROUP_ATTACHMENTS_TRUE      2009
+#define IDS_GROUP_ATTACHMENTS_FALSE     2010
+#define IDR_REPORTDIALOG_MENU           2011
+#define XTP_IDC_REPORT_MOVE             9556
+#define XTP_IDC_REPORT_RESIZE           9557
+#define ID_MODE_SNAP2LEFT               32771
+#define ID_VIEW_GROUPBOX                32771
+#define ID_MODE_SNAP2RIGHT              32772
+#define ID_MODE_SNAP2MARKER             32773
+#define ID_MODE_USE_SELECTION           32774
+#define ID_MODE_TIMEOFFSET              32775
+#define ID_VIEW_CLASSICSTYLE            32776
+#define ID_TRACKCONTROL_FLEXIBLEDRAG    32777
+#define ID_HEADER_ADDMARKER             32778
+#define ID_HEADER_REMOVEMARKER          32779
+#define ID_HEADER_DELETEALLMARKERS      32780
+#define ID_BLOCK_LOCKBLOCK              32781
+#define ID_BLOCK_REMOVEBLOCK            32782
+#define ID_ITEM_ADDBLOCK                32783
+#define ID_TRACKCONTROL_SNAPTOMARKERS   32784
+#define ID_TRACKCONTROL_SNAPTOBLOCKS    32785
+#define ID_ITEM_LOCKTRACK               32786
+#define ID_TRACKCONTROL_ALLOWBLOCKMOVE  32787
+#define ID_TRACKCONTROL_ALLOWBLOCKSCALE 32788
+#define ID_TRACKCONTROL_ALLOWROWRESIZE  32789
+#define ID_TRACKCONTROL_SCALEONRESIZE   32791
+#define ID_TRACKCONTROL_ALLOWBLOCKREMOVE 32793
+#define ID_TRACKCONTROL_SHOWWORKAREA    32794
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32795
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
