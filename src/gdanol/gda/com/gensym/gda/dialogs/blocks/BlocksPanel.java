1707442ca4162ac80bf944e83e45b70d2c983be4
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:08:07 +0400

    Commiting all

---
+++ src/gdanol/gda/com/gensym/gda/dialogs/blocks/BlocksPanel.java
@@ -0,0 +1,14 @@
+	
+package com.gensym.gda.dialogs.blocks;
+
+import com.gensym.message.Resource;
+
+
+public class BlocksPanel extends com.gensym.gda.dialogs.gdaPanel {
+
+	public BlocksPanel(BlocksDialog d) {
+    	super(d);
+    	i18n=Resource.getBundle("com.gensym.gda.dialogs.blocks.Messages");
+	}
+
+}
