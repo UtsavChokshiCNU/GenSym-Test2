aaf3b3245f0b96001483f6263dddc4609e6f4981
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:29:37 +0400

    Commiting all

---
+++ src/tests/performance/run-uc14.bat
@@ -0,0 +1,17 @@
+@echo off
+rem 
+rem %1 - Working copy (e.g. c:\g2) 
+rem      or installed version (e.g. "C:\Program Files (x86)\Gensym\g2-8.3r1") 
+rem      home directory
+rem %2 - nonempty argument means to run performance test against installer 
+rem      (e.g. installer)
+rem 
+
+setlocal
+
+set UC_ID=uc14
+set UC_KB=uc14.kb 
+set UC_ITERATIONS=130
+set UC_SHUTDOWN=true
+
+call run-uc.bat %1 %2
