1a3dd97f55832e7413b8d643375e034e159c3656
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:32:25 +0400

    Commiting all

---
+++ src/tw/c/tran.h
@@ -0,0 +1,20 @@
+/* tran.h -- Header File for tran.c
+ * Copyright (c) 1986 - 2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+/* Symbol Definitions */
+
+extern Object Pclos;
+extern Object Pclos_run;
+extern Object Pclosx;
+extern Object Pcl;
+extern Object Pcl_user;
+extern Object Pkeyword;
+extern Object Ptrun;
+extern Object Ptx;
+
+/* variables/constants */
+extern Object Dummy_place_for_chestnut_internals;
+extern Object Modules_to_translate_verbosely;
+extern Object The_standard_mnemonics;
