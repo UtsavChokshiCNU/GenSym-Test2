d3699229958f5f5a2b941ff47ea40ea474574682
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:45:09 +0400

    Commiting all

---
+++ src/cpplink/c/LinkAction.h
@@ -0,0 +1,36 @@
+
+/*----------------------------------------------------------------------
+CLASS
+    LinkAction
+
+KEYWORDS
+    Action Connect
+
+AUTHOR
+    Allan Scott
+
+DESCRIPTION
+    An Action that is not associated with a specific connection.
+    
+    Currently Connect is the only subclass although this may change.
+
+LAST MODIFIED
+
+    
+----------------------------------------------------------------------*/
+
+#ifndef _CPP_LINK_LINK_ACTION_H
+#define _CPP_LINK_LINK_ACTION_H
+
+#include "Action.h"
+
+namespace CppLink
+{
+
+class API LinkAction: public Action
+{  
+};
+
+}
+
+#endif // ifndef _CPP_LINK_LINK_ACTION_H
