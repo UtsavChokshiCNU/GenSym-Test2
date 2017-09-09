120f7c0417e8890dd6a3addbaf42fd7d144faa86
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:58:39 +0400

    Commiting all

---
+++ src/ext/c/messagebox.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+//
+
+// Dialog resource IDs for user icons
+#define IDI_MBU_ERROR		1246
+#define IDI_MBU_INFO		1247
+#define IDI_MBU_QUESTION	1248
+#define IDI_MBU_WARNING		1249
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         40008
+#define _APS_NEXT_CONTROL_VALUE         1041
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
