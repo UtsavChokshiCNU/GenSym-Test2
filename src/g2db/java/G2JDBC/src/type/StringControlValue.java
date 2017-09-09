db5eb228db79604711c4bc1595a8e55ea61fcd4f
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:18 +0400

    Commiting all

---
+++ src/g2db/java/G2JDBC/src/type/StringControlValue.java
@@ -0,0 +1,16 @@
+package type;
+
+/**
+ * This class contains string control keywords,
+ * used in the g2-database.kb
+ * @author SoftServe
+ *
+ */
+public class StringControlValue {
+	
+	public static final String EXECUTE = "EXEC";
+	
+	private StringControlValue() {
+		
+	}
+}
