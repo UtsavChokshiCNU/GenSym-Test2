39f304a81dcfd2c3bc7b808717ca927617326d63
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:58:35 +0400

    Commiting all

---
+++ src/ext/c/cppflags.h
@@ -0,0 +1,9 @@
+#ifndef _CPPFLAGS_INCLUDED  /* standard trick to prevent multiple inclusion */
+#define _CPPFLAGS_INCLUDED
+
+/* This file is now obsolete.  Use cprim.h, instead.  -jv, 5/19/05 */
+
+#include "cprim.h"
+
+
+#endif /* _CPPFLAGS_INCLUDED */
