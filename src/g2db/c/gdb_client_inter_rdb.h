200e50c217438c855c524f450a648285d5bd06eb
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:01:29 +0400

    Commiting all

---
+++ src/g2db/c/gdb_client_inter_rdb.h
@@ -0,0 +1,18 @@
+/*================================================================
+ *  GDB_CLIENT_INTER_RDB.H  - GDB Layer-3  (DEC RDB V5.1) 
+ *================================================================*/
+
+#ifndef GDB_CLIENT_INTER_RDB_H
+#define GDB_CLIENT_INTER_RDB_H
+
+/*-----------------------
+    globals
+  -----------------------*/
+
+  typedef struct SQLDA_PTRS {
+      gSQLDA     *bptr;
+      gSQLDA     *sptr;                 
+  } SQLDA_PTRS;
+
+
+#endif
