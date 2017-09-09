ae692a9aebe54feeb1afc2e2a64cd5f969f70d1e
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:48:32 +0400

    Commiting all

---
+++ src/xtp/Samples/Controls/FlatCombo/Resource.h
@@ -0,0 +1,50 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FlatCombo.rc
+//
+#define IDR_MANIFEST                    1
+#define IDM_ABOUTBOX                    0x0010
+#define IDS_ABOUTBOX                    101
+#define IDD_FLATCOMBO_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDC_GBOX_LIST                   1000
+#define IDC_GBOX_DOWN                   1001
+#define IDC_DROPLIST_COMBO              1002
+#define IDC_GBOX_EDIT                   1003
+#define IDC_DROPDOWN_COMBO              1004
+#define IDC_RADIO_THEME                 1005
+#define IDC_DROPLIST_DISABLE            1006
+#define IDC_DROPDOWN_DISABLE            1007
+#define IDC_DROPDOWN_AUTOCOMP           1008
+#define IDC_DROPLIST_TXT_CLR            1009
+#define IDC_DROPLIST_BAK_CLR            1010
+#define IDC_DROPDOWN_TXT_CLR            1011
+#define IDC_DROPDOWN_BAK_CLR            1012
+#define IDC_GBOX_APPEAR                 1013
+#define IDC_RADIO_THEME_DEFAULT         1014
+#define IDC_RADIO_THEME_OFFICE2000      1015
+#define IDC_RADIO_THEME_OFFICEXP        1016
+#define IDC_RADIO_THEME_OFFICE2003      1017
+#define IDC_RADIO_THEME_OFFICE2007      1018
+#define IDC_RADIO_THEME_OFFICE2010      1019
+#define IDC_RADIO_THEME_OFFICE2011      1020
+#define IDC_RADIO_THEME_WINDOWS7        1021
+#define IDC_RADIO_THEME_FLAT            1022
+#define IDC_RADIO_THEME_ULTRAFLAT       1023
+#define IDC_CHK_FLATBOARDERS            1024
+#define IDC_FLAT_EDIT                   1025
+#define IDC_RADIO_THEME_VSTUDIO2008     1026
+#define IDC_RADIO_THEME_VSTUDIO2010     1027
+#define IDC_COMBO1                      1028
+#define IDC_COMBO_COLOR                 1028
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1029
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
