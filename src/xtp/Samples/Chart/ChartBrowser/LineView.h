42e3b8997f5776f1a084cda875061f98dc41698f
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:47:49 +0400

    Commiting all

---
+++ src/xtp/Samples/Chart/ChartBrowser/LineView.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "BaseView.h"
+
+// CLineView form view
+
+class CLineView : public CBaseView
+{
+	DECLARE_DYNCREATE(CLineView)
+
+protected:
+	CLineView();           // protected constructor used by dynamic creation
+	virtual ~CLineView();
+
+public:
+	enum { IDD = IDD_VIEW_LINE };
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+#ifndef _WIN32_WCE
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	
+	BOOL m_bShowLabels;
+	BOOL m_bShowMarkers;
+	int m_nMarkerType;
+	int m_nMarkerSize;
+
+	void OnCheckShowLabels();
+	void OnCheckShowMarkers();
+	void OnCbnSelchangeComboMarkerSize();
+	void OnCbnSelchangeComboMarkerType();
+
+
+	void CreateChart();
+
+	void OnInitialUpdate();
+
+	void SetAxisTitle(CXTPChartDiagram2D* pDiagram, CString strAxisX, CString strAxisY);
+
+	DECLARE_MESSAGE_MAP()
+};
+
+
