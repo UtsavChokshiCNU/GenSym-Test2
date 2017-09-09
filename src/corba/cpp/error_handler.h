a24334903a72ed9b5d8cb6ecc174ea63a27995d1
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:45:03 +0400

    Commiting all

---
+++ src/corba/cpp/error_handler.h
@@ -0,0 +1,10 @@
+#if !defined(_ERROR_HANDLER_H_)
+#define _ERROR_HANDLER_H_
+
+#include <gsi_main.h>
+#include "utils.h"
+
+extern void	*error_handler(gsi_int error_context, gsi_int error_code, gsi_char *error_msg) ;
+
+#endif
+
