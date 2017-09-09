5fc9eb6095fcc7492a1f58025449cf33c1a08365
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:39:38 +0400

    Commit-1

---
+++ src/SymScale/c/user.h
@@ -0,0 +1,9 @@
+/* user.h -- User Header File
+ * Copyright (C) 1986-2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+#include "kernel.h"
+#include "uconfig.h"
+#include "uspecial.h"
+#include "userstrs.h"
