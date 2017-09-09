ad377c9512d766bdfc35ff1d920d945b7b2da8a0
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:49:14 +0400

    Commiting all

---
+++ src/xtp/Samples/ReportControl/ReportDataBinding/resource.h
@@ -0,0 +1,42 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ReportDataBinding.rc
+//
+#define IDR_MANIFEST                    1
+#define IDM_ABOUTBOX                    0x0010
+#define IDS_ABOUTBOX                    101
+#define IDD_REPORTDATABINDING_DIALOG    102
+#define IDR_MAINFRAME                   128
+#define IDR_POPUP_MENU                  129
+#define IDB_BMREPORT                    200
+#define IDC_TABLES                      1002
+#define IDC_RECORDS                     1003
+#define IDS_GROUP_DATE_OLDER            2000
+#define IDS_GROUP_DATE_THISYEAR         2001
+#define IDS_GROUP_DATE_THISMONTH        2002
+#define IDS_GROUP_DATE_TODAY            2003
+#define IDS_GROUP_CHECKED_TRUE          2004
+#define IDS_GROUP_CHECKED_FALSE         2005
+#define IDS_GROUP_IMPOPRTANCE_HIGH      2006
+#define IDS_GROUP_IMPOPRTANCE_NORMAL    2007
+#define IDS_GROUP_IMPOPRTANCE_LOW       2008
+#define IDS_GROUP_ATTACHMENTS_TRUE      2009
+#define IDS_GROUP_ATTACHMENTS_FALSE     2010
+#define IDC_OPEN_DB_BUTTON              2011
+#define IDC_ACTION_BUTTON               2012
+#define ID_ADD_REC_TO_RECSET            32771
+#define ID_DELETE_REC_FROM_RECSET       32772
+#define ID_ADD_RECORD                   32773
+#define ID_DELETE_RECORD                32774
+#define ID_SERVICE                      32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
