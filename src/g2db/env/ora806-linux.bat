55e7af3e9fc1121054d693229b6765eb8eb4faf2
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:06 +0400

    Commiting all

---
+++ src/g2db/env/ora806-linux.bat
@@ -0,0 +1,5 @@
+setenv ORACLE_HOME /gensym/spare/bridges/linux/dbs/oracle/806
+setenv ORA_NLS33   $ORACLE_HOME/ocommon/nls/admin/data
+setenv LD_LIBRARY_PATH /gensym/spare/bridges/linux/dbs/oracle/806/lib
+set    opath=$PATH
+setenv PATH $ORACLE_HOME/bin:$PATH
