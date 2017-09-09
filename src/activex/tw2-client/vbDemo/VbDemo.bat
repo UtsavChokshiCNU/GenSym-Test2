9b7fe9edc4f432c0710b8b2c85c8e59fd4efa9f9
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:42:29 +0400

    Commiting all

---
+++ src/activex/tw2-client/vbDemo/VbDemo.bat
@@ -0,0 +1,2 @@
+call "%SEQUOIA_HOME%\bin\start_g2" -kb "%SEQUOIA_HOME%\kbs\mill.kb" -name "Mill" -rgn1lmt 40000000 -width 600 -height 500 -tcpport 1111 -tcpipexact
+start "" "%SEQUOIA_HOME%\ActiveX\vbdemo\VbDemo.exe"
