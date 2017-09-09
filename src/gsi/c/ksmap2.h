0ce069d247f1ff297121b1a059ba91ec9edfe1c1
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:09:05 +0400

    Commiting all

---
+++ src/gsi/c/ksmap2.h
@@ -0,0 +1,37 @@
+/* ksmap2.h -- Header File for ksmap2.c
+ * Copyright (c) 1986 - 2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+/* Symbol Definitions */
+
+extern Object Pclos;
+extern Object Pclos_run;
+extern Object Pclosx;
+extern Object Pcl;
+extern Object Pcl_user;
+extern Object Pkeyword;
+extern Object Ptrun;
+extern Object Ptx;
+
+/* function declarations */
+
+#ifdef USE_PROTOTYPES
+
+extern Object g2int_gensym_ksc_code_to_contiguous(Object);
+extern Object g2int_make_cjk_code_mapping_vector(void);
+extern Object g2int_regenerate_optimized_constant(Object);
+extern Object g2int_set_cjk_code_mapping(Object,Object,Object);
+
+#else
+
+extern Object g2int_gensym_ksc_code_to_contiguous();
+extern Object g2int_make_cjk_code_mapping_vector();
+extern Object g2int_regenerate_optimized_constant();
+extern Object g2int_set_cjk_code_mapping();
+
+#endif
+
+/* variables/constants */
+extern Object G2int_ks_c_5601_to_unicode_map;
+extern Object G2int_unicode_to_ks_c_5601_map;
