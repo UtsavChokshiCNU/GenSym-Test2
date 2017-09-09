e1ff5f87e282f1a9e132c5e87f9c0ba0d480debc
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:34:36 +0400

    Commiting all

---
+++ src/twng/java/jfreelib/src/gensym/charts/scrolling/ScrollingListener.java
@@ -0,0 +1,12 @@
+package gensym.charts.scrolling;
+
+/**
+ * Scrolling listener interface
+ */
+public interface ScrollingListener {
+
+    /**
+     * Scrolled items callback
+     */
+    public void itemsScrolled();
+}
