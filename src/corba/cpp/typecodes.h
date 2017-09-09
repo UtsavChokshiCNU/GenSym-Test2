474895b475be8fd94c9115002611a4449feb0d21
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:45:05 +0400

    Commiting all

---
+++ src/corba/cpp/typecodes.h
@@ -0,0 +1,35 @@
+
+//
+// This file contains typecode defs
+//
+
+#define TK_NULL 0
+#define TK_VOID 1
+#define TK_SHORT 2
+#define TK_LONG 3
+#define TK_LONGLONG 23
+#define TK_USHORT 4
+#define TK_ULONG 5
+#define TK_ULONGLONG 24
+#define TK_FIXED 28
+#define TK_FLOAT 6
+#define TK_DOUBLE 7
+#define TK_LONGDOUBLE 25
+#define TK_BOOLEAN 8
+#define TK_CHAR 9
+#define TK_WCHAR 26
+#define TK_OCTET 10
+#define TK_ANY 11
+#define TK_TYPECODE 12
+#define TK_PRINCIPAL 13
+#define TK_OBJREF 14
+#define TK_STRUCT 15
+#define TK_UNION 16
+#define TK_ENUM 17
+#define TK_STRING 18
+#define TK_WSTRING 27
+#define TK_SEQUENCE 19
+#define TK_ARRAY 20
+#define TK_ALIAS 21
+#define TK_EXCEPT 22
+
