ddb24ed7ec6ebaf6bfadb22323c70d2030eea5a9
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:07 +0400

    Commiting all

---
+++ src/g2db/env/ora817-linux.bat
@@ -0,0 +1,5 @@
+setenv ORACLE_HOME /gensym/spare/bridges/linux/dbs/oracle/817
+setenv LD_LIBRARY_PATH /gensym/spare/bridges/linux/dbs/oracle/817/lib
+set opath=$PATH
+setenv PATH /gensym/spare/bridges/linux/dbs/oracle/817/bin:${PATH}
+
