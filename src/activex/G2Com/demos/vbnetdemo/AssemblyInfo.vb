b16a2e335a3aaae042b9bf46f12721dc2fa2e15e
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:42:20 +0400

    Commiting all

---
+++ src/activex/G2Com/demos/vbnetdemo/AssemblyInfo.vb
@@ -0,0 +1,33 @@
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly
+
+
+
+
+<Assembly: AssemblyTitle("VB.Net Demo")> 
+<Assembly: AssemblyDescription("Example of Using G2-ActiveXLink with VB.NET")> 
+<Assembly: AssemblyCompany("Gensym")>
+<Assembly: AssemblyProduct("VBNetDemo")> 
+<Assembly: AssemblyCopyright("Copyright, 2005, Gensym")> 
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Version information for an assembly consists of the following four values:
+
+'	Major version
+'	Minor Version
+'	Revision
+'	Build Number
+
+' You can specify all the values or you can default the Revision and Build Numbers
+' by using the '*' as shown below
+
+
+<Assembly:  AssemblyVersion("1.0.*")>
+
+
