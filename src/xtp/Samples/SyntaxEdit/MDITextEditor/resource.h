81e1c052f922e214547f8c9dfc661a27a67c7077
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:49:36 +0400

    Commiting all

---
+++ src/xtp/Samples/SyntaxEdit/MDITextEditor/resource.h
@@ -0,0 +1,43 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MDITextEditor.rc
+//
+#define IDR_MANIFEST                    1
+#define IDR_MAINFRAME                   128
+#define IDR_MDITEXTYPE                  129
+#define IDS_FILE_OPEN_FILTER            131
+#define IDD_SYNTAX_DLG                  133
+//#define IDC_SYNTAX_EDIT                 1009
+#define ID_FILE_GOTO                    32771
+#define ID_TOOLS_BOOKMARK               32773
+#define ID_TOOLS_BREAKPOINT             32774
+#define ID_TOOLS_NEXT_BOOKMARK          32777
+#define ID_TOOLS_PREV_BOOKMARK          32784
+#define ID_EDIT_TABIFY                  32789
+#define ID_EDIT_UNTABIFY                32790
+#define ID_EDIT_WHITESPACE              32791
+#define ID_TOOLS_OPTIONS                32792
+#define ID_EDIT_COLLAPSEALL             32793
+#define ID_EDIT_EXPANDALL               32794
+#define ID_EDIT_UPPERCASE               32796
+#define ID_EDIT_LOWERCASE               32798
+#define ID_EDIT_FIND_SILENT             32801
+#define ID_FILE_CLOSEALL                32803
+#define ID_FILE_SAVEALL                 32804
+#define ID_INDICATOR_ROWCOL             32805
+#define ID_EDIT_READONLY                32806
+#define ID_TOOLS_RELOADSYNTAX           32811
+#define ID_TOOLS_RELOADSYNTAX2INIMODE   32812
+#define ID_TOOLS_RELOADSYNTAXMODE       32813
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32813
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
