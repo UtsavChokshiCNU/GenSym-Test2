478c2a9b4f73af538ada9d4391518e93c7654127
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:30:06 +0400

    Commiting all

---
+++ src/tools/automation/Acceptance/BenchmarkSummarizer/Checker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BenchmarkSummarizer {
+    class Checker {
+        public static bool Must(bool allRight, object culprit) {
+            if( !allRight ) {
+                System.Console.WriteLine("\nCheck failed due to {0} at {1}\n",
+                    culprit.ToString(),
+                    Environment.StackTrace);
+            }
+            return allRight;
+        }
+    }
+}
