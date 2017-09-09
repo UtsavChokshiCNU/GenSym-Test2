6ed072948685550bb11a23775e4253d5c168efae
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:05:04 +0400

    Commiting all

---
+++ src/g2i/java/com/gensym/G2SNMP/trapd/BufferIOException.java
@@ -0,0 +1,18 @@
+package com.gensym.G2SNMP.trapd;
+
+class BufferIOException extends Exception {
+
+    /**
+       @roseuid 3678BB4801B1
+     */
+    public BufferIOException() {
+    }
+
+    /**
+       @roseuid 3678BB4801B2
+    */
+    public BufferIOException(String s) {
+        super(s);
+    }
+
+}
