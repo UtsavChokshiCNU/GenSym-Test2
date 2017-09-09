7313be655cba067958ac11e82c2cfdcbe282a7a8
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:58:38 +0400

    Commiting all

---
+++ src/ext/c/lispicp.h
@@ -0,0 +1,43 @@
+/* Auto-generated via Lisp function output-icp-protocol-features-for-c */
+/* Hand-editing is probably not advised. */
+
+#define ICP_MDI                        37
+#define ICP_CUSTOM_DIALOGS             38
+#define ICP_NMS_CONTROLS               39
+#define ICP_LIST_DIRECTORY             40
+#define ICP_SET_WINDOW_TITLE           41
+#define ICP_NMS_MODIFY_CONTROL         42
+#define ICP_MANAGE_STATUS_BAR          43
+#define ICP_NMS_SELECTION_CALLBACKS    44
+#define ICP_FILE_DIALOG_EX             45
+#define ICP_TREE_VIEWS                 46
+#define ICP_HTML_VIEWS                 47
+#define ICP_DEBUGGER_DIALOG            48
+#define ICP_NATIVE_EDITOR              49
+#define ICP_NATIVE_PRINTING_EX         50
+#define ICP_NMS_UNPOSTING_CALLBACKS    51
+#define ICP_SHORTCUT_BARS              52
+#define ICP_POLYARCS                   53
+#define ICP_CHART_VIEWS                54
+#define ICP_GRID_VIEWS                 55
+#define ICP_TABULAR_VIEW_SELECTED_ROWS 56
+#define ICP_ZORDER_NATIVE_WINDOW       57
+#define ICP_DIALOG_PANES               58
+#define ICP_NEW_GRID_VIEWS             59
+#define ICP_MANAGE_PANE                60
+#define ICP_SCROLL_WHILE_MINIMIZED_FIX 61
+#define ICP_NMS_SET_THEME              62
+#define ICP_TOOLBAR_DOCKING            63
+#define ICP_SIMPLE_DIALOG_EX           64
+#define ICP_TREE_VIEWS_EX              65
+#define ICP_VIEWS_API                  66
+#define ICP_HTML_HELP_EX               67
+#define ICP_MANAGE_UI_PROPERTIES       68
+#define ICP_MANAGE_LOGBOOK             69
+#define ICP_GRID_VIEW_SMALL_CHUNKS     70
+#define ICP_TABBED_MDI_MODE            71
+#define ICP_MANAGE_DIALOG              72
+#define ICP_MODIFY_NODE                73
+#define ICP_VIEWS_IN_DIALOGS           74
+#define ICP_HIDE_TOOLBAR               75
+#define ICP_EXTENDED_DIALOG_TEXT_BOXES 76
