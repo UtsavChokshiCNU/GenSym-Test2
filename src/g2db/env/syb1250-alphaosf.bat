974b0b738e35544b17b6967a9b444fdd336206be
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:10 +0400

    Commiting all

---
+++ src/g2db/env/syb1250-alphaosf.bat
@@ -0,0 +1,12 @@
+#!/bin/csh
+if ( $?PATH == "0" ) then
+	setenv PATH ""
+endif
+setenv PATH /gensym/bridges/alphaosf/dbs/sybase/1250/OCS-12_5/bin:$PATH
+setenv SYBASE_OCS OCS-12_5
+setenv SYBASE /gensym/bridges/alphaosf/dbs/sybase/1250
+setenv SYBASE_JRE /gensym/bridges/alphaosf/dbs/sybase/1250/shared-1_0/jre1.2.2
+if ( $?LD_LIBRARY_PATH == "0" ) then
+	setenv LD_LIBRARY_PATH ""
+endif
+setenv LD_LIBRARY_PATH /gensym/bridges/alphaosf/dbs/sybase/1250/OCS-12_5/lib:/gensym/bridges/alphaosf/dbs/sybase/1250/OCS-12_5/lib3p:/usr/lib:$LD_LIBRARY_PATH
