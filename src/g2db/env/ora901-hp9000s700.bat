ecd77263d08cdd2d7d716a278cb687a5fdd6b5b4
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:07 +0400

    Commiting all

---
+++ src/g2db/env/ora901-hp9000s700.bat
@@ -0,0 +1,3 @@
+setenv ORACLE_HOME     /gensym/bridges/hp9000s700/dbs/oracle/901
+setenv LD_LIBRARY_PATH /gensym/bridges/hp9000s700/dbs/oracle/901/lib32
+setenv SHLIB_PATH      /gensym/bridges/hp9000s700/dbs/oracle/901/lib32
