8d8865958861cd2a7948be3e70041c895bc7e5f7
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:42:23 +0400

    Commiting all

---
+++ src/activex/G2Com/resource.h
@@ -0,0 +1,42 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by G2Com.rc
+//
+#define IDS_PROJNAME                    100
+#define IDR_G2GATEWAY                   101
+#define IDR_GATEWAYPROP                 102
+#define IDR_G2STRUCTUREENTRY            103
+#define IDR_G2STRUCTURE                 104
+#define IDD_GATEWAYPROP                 105
+#define IDS_TITLEGatewayProp            106
+#define IDS_DOCSTRINGGatewayProp        107
+#define IDS_HELPFILEGatewayProp         108
+#define IDR_G2ITEM                      109
+#define IDR_G2ATTRIBUTE                 110
+#define IDR_G2LISTORARRAY               111
+#define IDR_G2SYMBOL                    112
+#define IDR_G2LAELEMENT                 113
+#define IDR_G2ITEMREF                   114
+#define IDR_G2WORKSPACE                 115
+#define IDR_G2WINDOW                    116
+#define IDR_G2COMCONFIGURATOR           117
+#define IDC_HOST                        201
+#define IDC_INTERFACECLASS              202
+#define IDI_ICON1                       202
+#define IDC_RPIS                        203
+#define IDC_TIMEOUT                     204
+#define IDC_G2CONNECT                   205
+#define IDC_KBSTART                     206
+#define IDC_G2SYMBOLS                   209
+#define IDC_DISCON_ON_RESET             210
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        215
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         210
+#define _APS_NEXT_SYMED_VALUE           118
+#endif
+#endif
