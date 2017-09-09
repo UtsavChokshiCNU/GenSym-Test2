081b1ada4e63f27db5dc1cd0e70fe1c772cb26ae
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:45:05 +0400

    Commiting all

---
+++ src/corba/cpp/yacc_supp.h
@@ -0,0 +1,27 @@
+/* ytab.h pre-defines */
+#define TYPE_DCL	0
+#define CONST_DCL	1
+#define EXCEPT_DCL	2
+#define ATTR_DCL	3
+#define OP_DCL		4
+#define	ENUM_TYPE	5
+
+#define IN_DIR		2
+#define OUT_DIR		6
+#define INOUT_DIR	7
+
+#define NO_OPATTRIBUTE	2
+#define YES_OPATTRIBUTE 3
+
+#define NO_READONLY		0
+#define	YES_READONLY	1
+
+#define RELATIVE_SCOPE	0
+#define GLOBAL_SCOPE	1
+
+#define DEBUG_OFF		0
+#define DEBUG_ON		1
+
+/* Compiler debug mode flag */
+extern int compiler_debug_state;
+
