a8e70f73b08e4d52e912835b35dfbf70151d68f9
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:44 +0400

    Commiting all

---
+++ src/g2i/doc/dev/standards/csv-file-format/csv file format_files/CSV01.js
@@ -0,0 +1,35 @@
+/*
+ * (C) Copyright 2003 Creativyst, Inc.
+*/
+
+
+/*
+ * Article: The CSV File Format
+ *
+ *  Resource Listings
+ *
+*/
+
+
+/*
+ * Individual links
+*/
+function CharacterCodeTutorial()
+{
+ var    s;
+
+ s = '';
+ s += ' ';
+
+s += '<a HREF="http://www.cs.tut.fi/~jkorpela/chars.html"';
+s += 'TARGET="_blank"';
+s += 'CLASS="LeftInfo"';
+s += 'TITLE="A wealth of intro and advanced info on character encoding"';
+s += '>Tutorial On Character Code Issues</A>';
+
+   document.write(s);
+}
+
+
+
+
