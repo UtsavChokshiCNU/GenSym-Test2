ec5543a9d2dc8f0cbeaf7be4b032aa9ebc7d6490
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:47:42 +0400

    Commiting all

---
+++ src/xanalys/c2/NativeKBext/Continental/StatisticalProcedures.c
@@ -0,0 +1,37 @@
+/** Accelerating functioality for Continental KB
+ * It involevs improving statistical functions used in KB level or even in lisp level
+ * Subsystem: G2.Subsystems.NativeKBext.Specific.Continental
+ * Tags:
+ *   - g2nke - native KB extensions
+ * Used In:
+ *   - The KB code of Continental
+ * 
+ * Copyright (C) 1986-2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+
+#include "StatisticalProcedures.h"
+
+Object XANALYS_API g2nke_historical_mean(
+	Object o_history, 
+	Object o_window_size, 
+	Object o_start_index)
+{
+	return NIL;
+}
+
+
+// Subject kb level procedure
+Object XANALYS_API g2nke_historical_variance(
+	Object o_history, 
+	Object o_window_size, 
+	Object o_start_index)
+{
+	return NIL;
+}
+
+
+
+
+
