91a9bbc3a914538ea9df7611081ed26761225cff
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:57 +0400

    Commiting all

---
+++ src/g2i/dotnet/WebSCOR/WebScorOCX/Resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by WebSCORax.rc
+//
+#define IDS_WEBSCORAX                   1
+#define IDD_ABOUTBOX_WEBSCORAX          1
+#define IDB_WEBSCORAX                   1
+#define IDI_ABOUTDLL                    1
+#define IDS_WEBSCORAX_PPG               2
+#define IDD_PROPPAGE_SMALL              102
+#define IDS_WEBSCORAX_PPG_CAPTION       200
+#define IDD_PROPPAGE_WEBSCORAX          200
+#define IDC_CHECK_USEPROXY              203
+#define IDC_EDIT_PROXY                  204
+#define IDC_EDIT_PORT                   205
+#define IDC_CHECK_USEAUTOCONFIG         206
+#define IDC_CHECK_USEAUTHENTICATION     207
+#define IDC_EDIT_USERNAME               208
+#define IDC_EDIT_PASSWORD               210
+#define IDC_EDIT_SCRIPT                 211
+#define IDC_EDIT_DOMAIN                 212
+#define IDC_RADIO1                      213
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         214
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
