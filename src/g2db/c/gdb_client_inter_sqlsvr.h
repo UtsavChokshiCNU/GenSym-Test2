dd18875585344671ff4b26ee7c206796255c6e47
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:01:29 +0400

    Commiting all

---
+++ src/g2db/c/gdb_client_inter_sqlsvr.h
@@ -0,0 +1,17 @@
+/*================================================================
+ *  GDB_CLIENT_INTER_SQLSVR.H  - GDB Layer-3
+ *================================================================*/
+
+#ifndef GDB_CLIENT_INTER_SQLSVR_H
+#define GDB_CLIENT_INTER_SQLSVR_H
+
+/*-----------------------
+    globals
+  -----------------------*/
+
+  typedef struct SQLDA_PTRS {
+      gSQLDA     *bptr;
+      gSQLDA     *sptr;                 
+  } SQLDA_PTRS;
+
+#endif
