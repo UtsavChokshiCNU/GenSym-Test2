ac002fd76c6ffd2a7b868d6f3d28d656e3b4a13f
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:06 +0400

    Commiting all

---
+++ src/g2db/env/ora806-intelnt.bat
@@ -0,0 +1,6 @@
+set ORACLE_HOME=k:\gensym\spare\bridges\intelnt\dbs\oracle\806
+set ORA_NLS33=%ORACLE_HOME%\NLSRTL33\DATA
+set opath=%PATH%
+set PATH=k:\gensym\spare\bridges\intelnt\dbs\oracle\806\bin;%PATH%
+rem call VCVARS32.bat
+
