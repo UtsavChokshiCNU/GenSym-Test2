ed6623631df8fd1a085750e6fa3b5a0e9b225d43
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:30:49 +0400

    Commiting all

---
+++ src/tools/BullseyeCoverage/run/libcov-windowsGraphic-entry.c
@@ -0,0 +1,13 @@
+/* $Revision: 5049 $ $Date: 2004-11-18 13:47:54 -0800 (Thu, 18 Nov 2004) $
+ * Copyright (c) Bullseye Testing Technology
+ * This source file contains confidential proprietary information.
+ *
+ * Purpose
+ *   Link this object to notify whether cov_DrvEnableDriver should hook
+ *   driver functions.
+ *
+ * Usage
+ *   Build -Dvalue=1 to enable hooking, -Dvalue=0 to disable
+ */
+
+int cov_graphicEntry = value;
