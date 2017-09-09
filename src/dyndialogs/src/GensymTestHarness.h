6ef8a10b8759ac624d86df34003558532d8cff64
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:58:11 +0400

    Commiting all

---
+++ src/dyndialogs/src/GensymTestHarness.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "ddrsc.h"
+
+#define OUR_DEFPUSHBUTTON	0
+#define OUR_GROUPBOX		1
+
+static void GensymTestHarness_OnCommand(HWND hWnd, int id, HWND hctl, UINT codeNotify);
+
