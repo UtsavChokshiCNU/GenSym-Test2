e32050562573751c4e6d8d32e65744d54e6982b7
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:39:35 +0400

    Commit-1

---
+++ src/SymScale/c/pshdr.h
@@ -0,0 +1,21 @@
+/* pshdr.h -- Header File for pshdr.c
+ * Copyright (C) 1986-2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+/* Symbol Definitions */
+extern Object Qmax;
+extern Object Qmin;
+
+extern Object Pclos;
+extern Object Pclos_run;
+extern Object Pclosx;
+extern Object Pcl;
+extern Object Pcl_user;
+extern Object Pkeyword;
+extern Object Ptrun;
+extern Object Ptx;
+
+/* variables/constants */
+extern Object Postscript_header_array;
+extern Object Postscript_paper_sizes;
