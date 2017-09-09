611faf717b5614fb8c0ad5af642204e20b313db2
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:17 +0400

    Commiting all

---
+++ src/g2db/java/G2JDBC/src/type/GDBControlValue.java
@@ -0,0 +1,17 @@
+package type;
+
+/**
+ * Holds GDB control values, as defined in the g2-database KB
+ * @author SoftServe
+ *
+ */
+public class GDBControlValue {
+
+	public static final int GDB_INIT = 1;
+	public static final int GDB_UPDATE = 2;
+	public static final int GDB_EXECUTE = 3;
+	
+	private GDBControlValue() {
+		
+	}
+}
