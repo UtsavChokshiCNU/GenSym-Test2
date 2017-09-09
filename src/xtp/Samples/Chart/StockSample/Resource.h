dd0a9a93694b4ef3abbead6ef1329f8e966f14be
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:47:53 +0400

    Commiting all

---
+++ src/xtp/Samples/Chart/StockSample/Resource.h
@@ -0,0 +1,42 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by StockSample.rc
+//
+#define IDR_MANIFEST                    1
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_STOCKSTYPE                  129
+#define IDD_STOCKQUOTE                  130
+#define IDC_STR_NAME                    1000
+#define IDC_STR_LASTTRADE               1001
+#define IDC_STR_TRADETIME               1002
+#define IDC_STR_CHANGE                  1003
+#define IDC_STR_PREVCLOSE               1004
+#define IDC_STR_OPEN                    1005
+#define IDC_STR_BID                     1006
+#define IDC_STR_ASK                     1007
+#define IDC_STR_1YTARGETEST             1008
+#define IDC_STR_DAYSRANGE               1009
+#define IDC_STR_52WKRANGE               1010
+#define IDC_STR_VOLUME                  1011
+#define IDC_STR_AVGVOL                  1012
+#define IDC_STR_MARKETCAP               1013
+#define IDC_COMBO_STYLE                 1014
+#define IDC_BTN_GETQUOTE                1017
+#define IDC_EDIT_SYMBOL                 1018
+#define ID_VIEW_STOCKQUOTE              32771
+#define ID_VIEW_THICKSTYLE              32772
+#define ID_VIEW_CANDLES                 32773
+#define ID_VIEW_DATETIMESCALE           32774
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
