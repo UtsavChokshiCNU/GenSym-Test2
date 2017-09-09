5b9ba8e7bc6e69a528d6f62266f73b9d67ec5904
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:05:17 +0400

    Commiting all

---
+++ src/g2i/java/test/com/gensym/jmail/JmailTest.java
@@ -0,0 +1,12 @@
+package com.gensym.jmail;
+
+import com.gensym.jmail.G2JavaLog;
+import junit.framework.TestCase;
+ 
+
+public class JmailTest extends TestCase {
+	public void testG2JavaLog(){
+		G2JavaLog g2javalog = new G2JavaLog();
+	}
+    
+}
