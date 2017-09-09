acc94ae7853a908f740d8fa095686a9311154540
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:06:31 +0400

    Commiting all

---
+++ src/g2i/sites/HLA/bridge_src/DataDistributionManagement.cpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *
+ *		DataDistributionManagement.c
+ *
+ *			Contains the G2 to GSI RPC functions related to HLA 
+ *			Data Distribution Management.
+ *
+ ***************************************************************************/
+
+
+
+#include	<stdlib.h>
+#include	<stdio.h>
+#include	<string.h>
+#include	<stdarg.h>
+#include	<errno.h>
+#include	<malloc.h>
+#include	<math.h>
+#include	<time.h>
+
+#include	<RTI.hh>
+#include	"G2Hla.h"
+
+
+/*-------------------------------------------------------------------------*
+ *
+ *	
+ *
+ *-------------------------------------------------------------------------*/
