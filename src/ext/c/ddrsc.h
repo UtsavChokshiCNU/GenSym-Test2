7941223d79982ef7401e134d77d9aeb81c67836c
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:58:36 +0400

    Commiting all

---
+++ src/ext/c/ddrsc.h
@@ -0,0 +1,69 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by GensymTestHarness.rc
+//
+#define IDC_LIST 201
+#define IDC_EDIT 202
+#define IDC_LABEL 203
+#define IDC_MYICON 206
+#define IDC_ANIMATE 207
+
+// Dialog resource IDs
+#define IDD_FILEDLG 1234
+#define IDD_MODULES 1237
+#define IDD_DIRDLG  1238
+
+// Copied from mfc/include/afxres.h
+#ifndef AFXDLL
+#define ID_EDIT_PASTE                   0xE125
+#define ID_EDIT_UNDO                    0xE12B
+#define ID_EDIT_REDO                    0xE12C
+#endif
+
+
+#define IDC_ABOUT_OSVERSION             1004
+
+
+
+#define IDC_EDIT1                       1000
+#define IDC_BUTTON1                     1011
+#define IDC_SPIN1                       1012
+#define IDC_TAB1                        1013
+#define IDC_BUTTON2                     1014
+#define IDC_BTN_DELETE                  1015
+#define IDC_LISTVIEW                    1016
+#define IDC_LISTVIEW_STATIC             1017
+#define IDC_COMBO1                      1018
+#define IDC_LIST_MSG_BROWSE             1018
+#define IDC_COMBO2                      1019
+#define IDC_COMBO3                      1020
+#define IDC_COMBO4                      1021
+#define IDC_STATIC_MSG_BROWSE           1022
+#define IDC_TAB4                        1023
+#define IDC_BTN_COLUMNS                 1023
+#define IDC_TAB5                        1024
+#define IDC_BTN_FILTERS                 1024
+#define IDC_STATIC_SORTDIR              1025
+#define IDC_BTN_ACK                     1025
+#define IDC_STATIC_SORTBY               1026
+#define IDC_STATIC_CUTOFF               1027
+#define IDC_STATIC_CUTOFFVAL            1028
+#define IDC_STATIC_CUTOFFDUR            1029
+#define IDC_STATIC_GENERAL              1031
+#define IDC_CHECK1                      1032
+#define IDC_TREE1                       1033
+
+#ifndef IDC_STATIC
+#  define IDC_STATIC                      (-1)
+#endif
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         40008
+#define _APS_NEXT_CONTROL_VALUE         1041
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
