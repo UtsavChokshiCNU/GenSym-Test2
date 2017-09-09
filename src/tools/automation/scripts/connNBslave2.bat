e0ddf5e7489c6040b65cd9f7227999f875caa36f
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:30:09 +0400

    Commiting all

---
+++ src/tools/automation/scripts/connNBslave2.bat
@@ -0,0 +1,1 @@
+call java -jar C:\G2\tools\automation\Acceptance\Imported\Hudson\slave.jar -jnlpUrl http://nb.gdev.com/hudson/computer/Gensym-Faster-Slave2/slave-agent.jnlp -jnlpCredentials nbjnlp:28WPv5oUp4bs
