b50bb873b5083e0252d7cf79ac8a0eaeec4bee04
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:22:26 +0400

    Commiting all

---
+++ src/scripts/linux64/make-g2-odbc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. setdirs.sh
+
+export PLATFM=linux64
+
+make_and_check() {
+    make -C $SRCBOX/g2db/mak -f $SRCBOX/g2db/mak/g2-odbc-3-linux.mak $1 $2 $3 $4 $5 $6 $7 $8
+    if [ $? -ne 0 ]; then
+	echo "make -C $SRCBOX/g2db/mak -f $SRCBOX/g2db/mak/g2-odbc-3-linux.mak $1 $2 failed"
+	exit 1
+    fi
+}
+
+make_and_check clean
+make_and_check `$SRCBOX/g2db/mak/gdefs` $@
