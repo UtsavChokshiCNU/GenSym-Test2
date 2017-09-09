af2041b864ced32034e96722fef678d1e6a211b2
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:34:39 +0400

    Commiting all

---
+++ src/twng/java/jfreelib/src/gensym/charts/SourceXYZDataset.java
@@ -0,0 +1,10 @@
+package gensym.charts;
+
+import org.jfree.data.xy.IntervalXYDataset;
+import org.jfree.data.xy.XYZDataset;
+
+/**
+ * Source interface of the XYZ dataset
+ */
+public interface SourceXYZDataset extends XYZDataset, IntervalXYDataset {
+}
