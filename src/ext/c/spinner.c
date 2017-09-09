2671ee3a37f091a82ccfba013654f3fcf21f8eeb
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:58:40 +0400

    Commiting all

---
+++ src/ext/c/spinner.c
@@ -0,0 +1,43 @@
+/*-*- Mode: C++ -*-*/
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++
++ Module:      spinner.c
++
++ Copyright (C) 1986-2017 Gensym Corporation.
++            All Rights Reserved.
++
++ Author(s):
++   MS Windows portions:
++              2003 - 2005      jv, hjg, lkasparek, mhd, ajs
++
++ Description:  Implementation of native spinner in Telewindows
++
++ Key:
++   (obsolete)
++
++ File Organization:
++   Section:      INCLUDE FILES
++   Section:      DEFINES
++   Section:      GLOBALS
++   Section:      FUNCTION GROUP
++
++ External Interface:
++
++ Dependencies:
++
++ Notes:
++   This file is only compiled on Microsoft Windows.
++
++ Modifications:
++   usage: All modifications to this file should be listed here, with most
++          recent first. In general, modification comments should merely
++          provide a reference to a detailed modification comment in the
++          appropriate section/function header.
++
++   5/26/05,  jv: Created, initially from code committed to w32_dyndlg.cpp in
++                   the past two years.
++
++
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/* empty, unused file -- to be "cvs remove"d soon */
