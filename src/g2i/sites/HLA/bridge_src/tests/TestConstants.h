6ad554ec9eefb1767941f1cd640f7d9d2e81aeae
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:06:33 +0400

    Commiting all

---
+++ src/g2i/sites/HLA/bridge_src/tests/TestConstants.h
@@ -0,0 +1,20 @@
+#ifndef _TEST_CONSTANTS_H_
+#define _TEST_CONSTANTS_H_
+
+#include "stdafx.h"
+#include "G2Hla.h"
+#include "gsi_main.h"
+
+// Global variables declaration
+extern gsi_item* local_gsi_itemP;
+extern void* arguments[];
+extern const char* gla_home_variable;
+extern const char* errorResult;
+extern BOOL G2_joined_Federation;
+
+extern bool createIsOk;
+extern bool joinIsOk;
+extern bool isErrorResult;
+
+#endif //_TEST_CONSTANTS_H_
+
