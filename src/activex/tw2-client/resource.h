f07bf97b37d83bad9ec46e9a92f62c8226fcbffa
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:42:28 +0400

    Commiting all

---
+++ src/activex/tw2-client/resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by AXClient.rc
+//
+#define IDS_PROJNAME                    100
+#define IDR_G2UIGATEWAY                 101
+#define IDR_STRUCTURE                   102
+#define IDS_G2STRUCTURE_DESC            103
+#define IDR_G2Structure                 104
+#define IDS_SYMBOL_DESC                 105
+#define IDR_Symbol                      106
+#define IDS_G2SYMBOL_DESC               107
+#define IDR_G2Symbol                    108
+#define IDR_G2ITEM                      109
+#define IDR_G2WORKSPACEPANEL            110
+#define IDR_G2ITEMHOLDER                111
+#define IDB_BITMAP1                     204
+#define IDB_BITMAP2                     205
+#define IDB_BITMAP3                     206
+#define IDB_BITMAP4                     207
+#define IDI_ICON1                       208
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        209
+#define _APS_NEXT_COMMAND_VALUE         32770
+#define _APS_NEXT_CONTROL_VALUE         201
+#define _APS_NEXT_SYMED_VALUE           111
+#endif
+#endif
