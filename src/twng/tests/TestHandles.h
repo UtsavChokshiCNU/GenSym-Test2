497dc2f8033e656e290b7a7c3a7180651beccff8
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:35:21 +0400

    Commiting all

---
+++ src/twng/tests/TestHandles.h
@@ -0,0 +1,25 @@
+#ifndef _TEST_HANDLES_H_
+#define _TEST_HANDLES_H_
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+/*
+ * Class that contains G2 hanles tests
+ */
+
+class TestHandles : public CPPUNIT_NS::TestFixture  
+{
+
+    CPPUNIT_TEST_SUITE(TestHandles);
+		CPPUNIT_TEST(test);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+	virtual void setUp();
+	virtual void tearDown();
+
+	void test();
+};
+
+#endif
