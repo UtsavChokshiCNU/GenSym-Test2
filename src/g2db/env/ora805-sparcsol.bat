dbc5cf36d25dd6ae09060cbe408b60f9beaa868a
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:06 +0400

    Commiting all

---
+++ src/g2db/env/ora805-sparcsol.bat
@@ -0,0 +1,4 @@
+setenv ORACLE_HOME /gensym/spare/bridges/sparcsol/dbs/oracle/805
+setenv OLDLIBPATH "$LD_LIBRARY_PATH"
+# setenv LD_LIBRARY_PATH {$LD_LIBRARY_PATH}:/gensym/spare/bridges/sparcsol/dbs/oracle/805/lib
+setenv LD_LIBRARY_PATH {$LD_LIBRARY_PATH}:/gensym/spare/bridges/sparcsol/dbs/oracle/805/lib:/gensym/spare/bridges/sparcsol/lib
