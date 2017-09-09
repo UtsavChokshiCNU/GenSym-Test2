8f8409ebee3f957fc036b3c1ebe96b9c8772f791
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:47:52 +0400

    Commiting all

---
+++ src/xtp/Samples/Chart/ChartDialog/StdAfx.cpp
@@ -0,0 +1,26 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	ChartDialog.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+//
+// --------------------------------------------------------------------------
+// COPYRIGHT NOTICE:
+// --------------------------------------------------------------------------
+// 
+// "Codejock Chart" is a MFC extension library for creating modern charting 
+// applications. This file and the code herein are part of the Codejock Chart
+// MFC extension library.
+//
+// This code is protected under U.S. Copyright Law and International treaties
+// and is intended for registered and licensed use only. Any other use is 
+// strictly prohibited. This code is subject to the terms and conditions 
+// outlined in the "Codejock Chart" End User License Agreement (EULA).
+//
+// Copyright (c) 1998-2011 Codejock Technologies LLC, All Rights Reserved.
+// http://www.codejock.com
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+
+
+
