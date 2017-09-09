0ca7f42dac1b43120532e40320ba06292b0ca388
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:49:04 +0400

    Commiting all

---
+++ src/xtp/Samples/FlowGraph/FlowGraphSample/Resource.h
@@ -0,0 +1,72 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by FlowGraphSample.rc
+//
+#define IDR_MANIFEST                    1
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_FLOWGRTYPE                  129
+#define IDR_MENU_CONTEXT                130
+#define IDD_DIALOG_PERFOMANCE           131
+#define IDD_DIALOG_CONTROLS             132
+#define IDD_DIALOG_PAGETRANSITION       133
+#define IDR_TOOLBAR_ALPHA               160
+#define IDC_GRAPHCONTROL                1000
+#define IDC_BUTTON_PREV                 1001
+#define IDC_BUTTON_NEXT                 1002
+#define IDC_STATIC_PAGE                 1003
+#define IDC_BUTTON_ARRANGE              1004
+#define IDC_COMBO_SMOOTHING_MODE        1005
+#define IDB_NODEIMAGE_CUSTOMERS         1008
+#define IDB_NODEIMAGE_ORDERDETAILS      1009
+#define ID_EDIT_ARRANGE                 32772
+#define ID_NODE_RENAME                  32773
+#define ID_NODE_DELETE                  32774
+#define ID_NODE_ADD                     32775
+#define ID_FLOWGRAPH_TEST               32776
+#define ID_TEST_GRAPHWITH500NODES       32777
+#define ID_TEST_NODESWITHCONTROLS       32778
+#define ID_TEST_PAGETRANSITION          32779
+#define ID_NODE_GROUP                   32780
+#define ID_GROUP_GROUP                  32781
+#define ID_GROUP_UNFROUP                32782
+#define ID_GROUP_UNGROUP                32783
+#define ID_FLOWGRAPH_METALLICTHEME      32784
+#define ID_FLOWGRAPH_SHOWGRID           32785
+#define ID_NODE_COLOR_BLUE              32786
+#define ID_NODE_COLOR_RED               32787
+#define ID_NODE_COLOR_GREEN             32788
+#define ID_NODE_COLOR_PURPLE            32789
+#define ID_NODE_COLOR_AQUA              32790
+#define ID_NODE_COLOR_ORANGE            32791
+#define ID_CONNECTORTYPE_STRAIGHTCONNECTOR 32793
+#define ID_CONNECTORTYPE_CURVEDCONNECTOR 32794
+#define ID_CONNECTION_DELETECONNECTION  32796
+#define ID_CONNECTION_CLEANCONTROLPOINTS 32797
+#define ID_FLOWGRAPH_STRAIGHTCONNECTOR  32800
+#define ID_FLOWGRAPH_CURVEDCONNECTOR    32801
+#define ID_FLOWGRAPH_SHOWNODEGROUPSALWAYS 32802
+#define ID_CONNECTIONPOINT_RENAME       32803
+#define ID_FLOWGRAPH_QUALITY_HIGHQUALITY 32804
+#define ID_FLOWGRAPH_QUALITY_HIGHSPEED  32805
+#define ID_FLOWGRAPH_QUALITY_GDISTANDARD 32806
+#define ID_FLOWGRAPH_QUALITY_AUTOQUALITY 32807
+#define ID_CONNECTIONPOINT_DELETE       32808
+#define ID_CONNECTIONPOINT_ADD          32809
+#define ID_VIEW_ZOOM                    57645
+#define ID_INDICATOR_ZOOMSLIDER         57646
+#define ID_INDICATOR_ZOOM               57647
+#define ID_VIEW_ZOOM_TOSELECTION        57648
+#define ID_VIEW_ZOOM_FITWINDOW          57649
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32810
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
