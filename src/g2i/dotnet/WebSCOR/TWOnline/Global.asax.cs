88efc86a9f996b96984831ddf0699206a763a625
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:48 +0400

    Commiting all

---
+++ src/g2i/dotnet/WebSCOR/TWOnline/Global.asax.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Web;
+using System.Web.SessionState;
+using System.Configuration;
+
+namespace TWOnline 
+{
+	/// <summary>
+	/// Summary description for Global.
+	/// </summary>
+	public class Global : System.Web.HttpApplication
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		public Global()
+		{
+			InitializeComponent();
+		}	
+		
+		protected void Application_Start(Object sender, EventArgs e)
+		{
+			Context.Cache["CONN_STR"] = ConfigurationSettings.AppSettings["connString"];
+			Context.Cache["OCX_CODE_BASE"] = ConfigurationSettings.AppSettings["OcxCodeBase"];
+			Context.Cache["OCX_CLASS_ID"] = ConfigurationSettings.AppSettings["OcxClassID"];
+		}
+ 
+		protected void Session_Start(Object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_BeginRequest(Object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_EndRequest(Object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_Error(Object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Session_End(Object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_End(Object sender, EventArgs e)
+		{
+
+		}
+			
+		#region Web Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{    
+			this.components = new System.ComponentModel.Container();
+		}
+		#endregion
+	}
+}
+
