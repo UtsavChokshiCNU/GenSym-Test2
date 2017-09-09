72921bbcc28e0c969ef7d4376b199153a0644dfd
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:33 +0400

    Commiting all

---
+++ src/g2i/bin/del-protools-doc.bat
@@ -0,0 +1,188 @@
+echo off
+
+rem
+rem	Batch file to delete existing protools documentation
+rem
+
+setlocal
+set G2IHOME=C:\bt\G2i
+
+cd %G2iHOME%\doc\bpr
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\cdg
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\cdggrlb
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\equipmnt
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\escor-customiz
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gdev
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gdoc
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gdpm
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gedp
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gerr
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gevm
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gmib
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gndo
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gope
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\grlb
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\grpe
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\grtl
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gtad
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gtrap
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\guif
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gvad
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\gweb
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\jmail
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\jms
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\methods
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\methods-online
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\optegrity-core
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\sdb
+del *.htm
+del *.dat
+
+
+cd %G2iHOME%\doc\iato
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\icco
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\ico
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\icso
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\idco
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\iedo
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\iffo
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\iho
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\ioc
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\ipo
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\irco
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\iso
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\ito
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\ivo
+del *.htm
+del *.dat
+
+cd %G2iHOME%\doc\ixo
+del *.htm
+del *.dat
+
+
+cd %G2iHOME%\doc\protools
+del *.htm
+del *.dat
+
+cd %G2iHOME%\bin
