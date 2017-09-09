955af8b3d3406d589d71690e46a606ed6be12720
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:15 +0400

    Commiting all

---
+++ src/g2db/java/G2JDBC/src/com/gensym/classes/modules/g2database/DbTriggerObjectImpl.java
@@ -0,0 +1,404 @@
+/*
+ *  Copyright (C) 1986-2017 Gensym Corporation.
+ *  All Rights Reserved.
+ *
+ *      DbTriggerObjectImpl.java
+ *
+ *   Description: Generated Interface file. Do not edit!
+ *
+ *        Author: Gensym Corp.
+ *
+ *       Version: 5.1 Rev. 1
+ *
+ *          Date: Thu Jun 18 17:48:05 EEST 2009
+ *
+ */
+
+
+package com.gensym.classes.modules.g2database;
+
+import com.gensym.classes.*;
+import com.gensym.util.Structure;
+import com.gensym.util.Symbol;
+import com.gensym.util.symbol.SystemAttributeSymbols;
+import com.gensym.jgi.*;
+
+public class DbTriggerObjectImpl extends com.gensym.classes.GsiDataServiceImpl implements DbTriggerObject {
+
+
+  static final long serialVersionUID = 2L;
+  private static final Symbol OBJECT_HANDLE_ = Symbol.intern ("OBJECT-HANDLE");
+  private static final Symbol TIME_STAMP_ = Symbol.intern ("TIME-STAMP");
+
+  /* Generated constructors */
+
+  public DbTriggerObjectImpl() {
+    super();
+  }
+
+  public DbTriggerObjectImpl(G2Access context, int handle, Structure attributes) {
+    super (context, handle, attributes);
+  }
+
+
+  public DbTriggerObjectImpl(G2Access context, int handle, int handleVersion, G2__BaseImpl wrap, Structure attributes) {
+    super (context, handle, handleVersion, wrap, attributes);
+  }
+
+
+  /**
+   * Generated Property Setter for attribute -- OBJECT-HANDLE
+   * @param objectHandle new value to conclude for OBJECT-HANDLE
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setObjectHandle(int objectHandle) throws G2AccessException {
+    setAttributeValue (OBJECT_HANDLE_, objectHandle);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- OBJECT-HANDLE
+   * @return the value of the OBJECT-HANDLE attribute of this item
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public int getObjectHandle() throws G2AccessException {
+    java.lang.Object retnValue = getAttributeValue (OBJECT_HANDLE_);
+    return ((Integer)retnValue).intValue ();
+  }
+
+  /**
+   * Generated Property Setter for attribute -- TIME-STAMP
+   * @param timeStamp new value to conclude for TIME-STAMP
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setTimeStamp(java.lang.String timeStamp) throws G2AccessException {
+    setAttributeValue (TIME_STAMP_, timeStamp);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- TIME-STAMP
+   * @return the value of the TIME-STAMP attribute of this item
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.String getTimeStamp() throws G2AccessException {
+    java.lang.Object retnValue = getAttributeValue (TIME_STAMP_);
+    return (java.lang.String)retnValue;
+  }
+
+  /**
+   * Generated Property Setter for attribute -- INITIAL-VALUE
+   * @param initialValue new value to conclude for INITIAL-VALUE
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setInitialValue(java.lang.String initialValue) throws G2AccessException {
+    setAttributeValue (SystemAttributeSymbols.INITIAL_VALUE_, initialValue);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- INITIAL-VALUE
+   * @return the value of the INITIAL-VALUE attribute of this item
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.String getInitialValue() throws G2AccessException {
+    java.lang.Object retnValue = getAttributeValue (SystemAttributeSymbols.INITIAL_VALUE_);
+    return (java.lang.String)retnValue;
+  }
+
+  /**
+   * Generated Property Setter for attribute -- AUTHORS
+   * @param authors new value to conclude for AUTHORS
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setAuthorsForClass(com.gensym.util.Structure authors) throws G2AccessException {
+    setStaticAttributeValue (SystemAttributeSymbols.AUTHORS_, authors);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- AUTHORS
+   * @return the value of the AUTHORS attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public com.gensym.util.Structure getAuthorsForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.AUTHORS_);
+    return (com.gensym.util.Structure)retnValue;
+  }
+
+  /**
+   * Generated Property Getter for attribute -- CHANGE-LOG
+   * @return the value of the CHANGE-LOG attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public com.gensym.util.Sequence getChangeLogForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.CHANGE_LOG_);
+    return (com.gensym.util.Sequence)retnValue;
+  }
+
+  /**
+   * Generated Property Setter for attribute -- CLASS-NAME
+   * @param className new value to conclude for CLASS-NAME
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setClassNameForClass(com.gensym.util.Symbol className) throws G2AccessException {
+    setStaticAttributeValue (SystemAttributeSymbols.CLASS_NAME_, className);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- CLASS-NAME
+   * @return the value of the CLASS-NAME attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public com.gensym.util.Symbol getClassNameForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.CLASS_NAME_);
+    return (com.gensym.util.Symbol)retnValue;
+  }
+
+  /**
+   * Generated Property Setter for attribute -- CLASS-SPECIFIC-ATTRIBUTES
+   * @param classSpecificAttributes new value to conclude for CLASS-SPECIFIC-ATTRIBUTES
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setClassSpecificAttributesForClass(com.gensym.util.Sequence classSpecificAttributes) throws G2AccessException {
+    setStaticAttributeValue (SystemAttributeSymbols.CLASS_SPECIFIC_ATTRIBUTES_, classSpecificAttributes);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- CLASS-SPECIFIC-ATTRIBUTES
+   * @return the value of the CLASS-SPECIFIC-ATTRIBUTES attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public com.gensym.util.Sequence getClassSpecificAttributesForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.CLASS_SPECIFIC_ATTRIBUTES_);
+    return (com.gensym.util.Sequence)retnValue;
+  }
+
+  /**
+   * Generated Property Getter for attribute -- CLASS-RESTRICTIONS
+   * @return the value of the CLASS-RESTRICTIONS attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.Object getClassRestrictionsForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.CLASS_RESTRICTIONS_);
+    return (java.lang.Object)retnValue;
+  }
+
+  /**
+   * Generated Property Setter for attribute -- INSTANCE-CONFIGURATION
+   * @param instanceConfiguration new value to conclude for INSTANCE-CONFIGURATION
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setInstanceConfigurationForClass(com.gensym.util.Sequence instanceConfiguration) throws G2AccessException {
+    setStaticAttributeValue (SystemAttributeSymbols.INSTANCE_CONFIGURATION_, instanceConfiguration);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- INSTANCE-CONFIGURATION
+   * @return the value of the INSTANCE-CONFIGURATION attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public com.gensym.util.Sequence getInstanceConfigurationForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.INSTANCE_CONFIGURATION_);
+    return (com.gensym.util.Sequence)retnValue;
+  }
+
+  /**
+   * Generated Property Getter for attribute -- CLASS-INHERITANCE-PATH
+   * @return the value of the CLASS-INHERITANCE-PATH attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public com.gensym.util.Sequence getClassInheritancePathForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.CLASS_INHERITANCE_PATH_);
+    return (com.gensym.util.Sequence)retnValue;
+  }
+
+  /**
+   * Generated Property Getter for attribute -- INHERITED-ATTRIBUTES
+   * @return the value of the INHERITED-ATTRIBUTES attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public com.gensym.util.Sequence getInheritedAttributesForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.INHERITED_ATTRIBUTES_);
+    return (com.gensym.util.Sequence)retnValue;
+  }
+
+  /**
+   * Generated Property Getter for attribute -- SUPERIOR-CLASS
+   * @return the value of the SUPERIOR-CLASS attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.Object getSuperiorClassForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.SUPERIOR_CLASS_);
+    return (java.lang.Object)retnValue;
+  }
+
+  /**
+   * Generated Property Getter for attribute -- CAPABILITIES-AND-RESTRICTIONS
+   * @return the value of the CAPABILITIES-AND-RESTRICTIONS attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.Object getCapabilitiesAndRestrictionsForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.CAPABILITIES_AND_RESTRICTIONS_);
+    return (java.lang.Object)retnValue;
+  }
+
+  /**
+   * Generated Property Getter for attribute -- ATTRIBUTES-SPECIFIC-TO-CLASS
+   * @return the value of the ATTRIBUTES-SPECIFIC-TO-CLASS attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.Object getAttributesSpecificToClassForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.ATTRIBUTES_SPECIFIC_TO_CLASS_);
+    return (java.lang.Object)retnValue;
+  }
+
+  /**
+   * Generated Property Getter for attribute -- MENU-OPTION
+   * @return the value of the MENU-OPTION attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.Object getMenuOptionForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.MENU_OPTION_);
+    return (java.lang.Object)retnValue;
+  }
+
+  /**
+   * Generated Property Setter for attribute -- DIRECT-SUPERIOR-CLASSES
+   * @param directSuperiorClasses new value to conclude for DIRECT-SUPERIOR-CLASSES
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setDirectSuperiorClassesForClass(com.gensym.util.Sequence directSuperiorClasses) throws G2AccessException {
+    setStaticAttributeValue (SystemAttributeSymbols.DIRECT_SUPERIOR_CLASSES_, directSuperiorClasses);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- DIRECT-SUPERIOR-CLASSES
+   * @return the value of the DIRECT-SUPERIOR-CLASSES attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public com.gensym.util.Sequence getDirectSuperiorClassesForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.DIRECT_SUPERIOR_CLASSES_);
+    return (com.gensym.util.Sequence)retnValue;
+  }
+
+  /**
+   * Generated Property Getter for attribute -- CHANGE
+   * @return the value of the CHANGE attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.Object getChangeForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.CHANGE_);
+    return (java.lang.Object)retnValue;
+  }
+
+  /**
+   * Generated Property Setter for attribute -- ATTRIBUTE-INITIALIZATIONS
+   * @param attributeInitializations new value to conclude for ATTRIBUTE-INITIALIZATIONS
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setAttributeInitializationsForClass(com.gensym.util.Sequence attributeInitializations) throws G2AccessException {
+    setStaticAttributeValue (SystemAttributeSymbols.ATTRIBUTE_INITIALIZATIONS_, attributeInitializations);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- ATTRIBUTE-INITIALIZATIONS
+   * @return the value of the ATTRIBUTE-INITIALIZATIONS attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public com.gensym.util.Sequence getAttributeInitializationsForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.ATTRIBUTE_INITIALIZATIONS_);
+    return (com.gensym.util.Sequence)retnValue;
+  }
+
+  /**
+   * Generated Property Setter for attribute -- ATTRIBUTE-DISPLAYS
+   * @param attributeDisplays new value to conclude for ATTRIBUTE-DISPLAYS
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setAttributeDisplaysForClass(java.lang.Object attributeDisplays) throws G2AccessException {
+    setStaticAttributeValue (SystemAttributeSymbols.ATTRIBUTE_DISPLAYS_, attributeDisplays);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- ATTRIBUTE-DISPLAYS
+   * @return the value of the ATTRIBUTE-DISPLAYS attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.Object getAttributeDisplaysForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.ATTRIBUTE_DISPLAYS_);
+    return (java.lang.Object)retnValue;
+  }
+
+  /**
+   * Generated Property Setter for attribute -- STUBS
+   * @param stubs new value to conclude for STUBS
+   * @exception G2AccessException if there are any communication problems
+   *              or the value does not match with the type specification
+   */
+  @Override
+  public void setStubsForClass(java.lang.Object stubs) throws G2AccessException {
+    setStaticAttributeValue (SystemAttributeSymbols.STUBS_, stubs);
+  }
+
+  /**
+   * Generated Property Getter for attribute -- STUBS
+   * @return the value of the STUBS attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.Object getStubsForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.STUBS_);
+    return (java.lang.Object)retnValue;
+  }
+
+  /**
+   * Generated Property Getter for attribute -- DEFAULT-SETTINGS
+   * @return the value of the DEFAULT-SETTINGS attribute of this class
+   * @exception G2AccessException if there are any communication problems
+   */
+  @Override
+  public java.lang.Object getDefaultSettingsForClass() throws G2AccessException {
+    java.lang.Object retnValue = getStaticAttributeValue (SystemAttributeSymbols.DEFAULT_SETTINGS_);
+    return (java.lang.Object)retnValue;
+  }
+
+  private static String NoBodyExceptionString = "This method has no implementation for local access";
+  
+  
+}
