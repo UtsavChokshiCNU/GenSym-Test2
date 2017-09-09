6460f38dd0b69be096357baf4465814f3bfed9d2
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:28:30 +0400

    Commiting all

---
+++ src/tests/bench/gsi-3-0/gsi_user.h
@@ -0,0 +1,26 @@
+/* ****************************************************************
+
+   File: gsi_user.h
+
+   This file contains all of the user specific codes for the  user
+   written code. 
+
+ *****************************************************************/
+#define MAXSTRSZ 128
+#define MAXSENSORS 22
+
+/*#define GSI_PORT_NUM NO_SEL_PORT*/
+#define GSI_PORT_NUM  0
+
+
+
+#define WITH_RPCS 1
+
+
+
+#define FOR_VECTOR TRUE
+#define NUMBER_OF_VECTOR_ELEMENTS  9
+
+/*#define DEBUG_VECTOR TRUE*/
+
+
