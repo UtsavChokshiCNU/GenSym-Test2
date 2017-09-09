37bfd1b38c5853efe2c1529f604e61f2fff6c537
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:05:06 +0400

    Commiting all

---
+++ src/g2i/java/com/gensym/JMibParser/G2Version.java
@@ -0,0 +1,11 @@
+package com.gensym.JMibParser;
+
+public class G2Version {
+
+  public static final String VERSION_INFORMATION = "Version 1.0 Rev. 1";
+  public static final String BUILD_INFORMATION = "1000120";
+  public static final String BUILD_DATE = "16 Apr 2007  11:43:10 a.m.";
+  public static final String COPYRIGHT_INFORMATION = "Gensym Corporation 2002-2007";
+  public static final String INTEGRITY_INFORMATION = "Built for Integrity 5.0 Rev. 0";
+ 
+}
