5d7bcc6cf41a2753436cf32416ad620112ef766e
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:47:50 +0400

    Commiting all

---
+++ src/xtp/Samples/Chart/ChartBrowser/Resource.h
@@ -0,0 +1,107 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ChartBrowser.rc
+//
+#define IDR_MANIFEST                    1
+#define IDR_MAINFRAME                   128
+#define IDR_CHARTBTYPE                  129
+#define IDD_VIEW_BAR                    130
+#define IDD_VIEW_POINT                  131
+#define IDD_VIEW_BUBBLE                 132
+#define IDD_VIEW_LINE                   133
+#define IDD_VIEW_SPLINE                 134
+#define IDD_VIEW_AREA                   135
+#define IDD_VIEW_SPLINEAREA             136
+#define IDD_VIEW_PIE                    137
+#define IDD_VIEW_DOUGHNUT               138
+#define IDD_VIEW_PIE3D                  139
+#define IDD_VIEW_DOUGHNUT3D             140
+#define IDD_VIEW_TORUS3D                141
+#define IDD_VIEW_HIGHLOW                142
+#define IDD_VIEW_CANDLESTICK            143
+#define IDD_VIEW_STACKEDBAR             144
+#define IDD_VIEW_AREA1                  145
+#define IDD_VIEW_STACKEDAREA            145
+#define IDD_VIEW_BAR_AND_LINE           146
+#define IDD_VIEW_SIMPLE                 147
+#define IDD_VIEW_TITLE                  148
+#define IDD_VIEW_DIAGRAMS               149
+#define IDD_VIEW_STEPLINE               150
+#define IDD_VIEW_RANGEBAR               151
+#define IDD_VIEW_SECONDARYAXES          152
+#define IDD_VIEW_MULTIPLEVIEW           153
+#define IDD_VIEW_ZOOMANDSCROLL          154
+#define IDD_VIEW_FUNNEL                 155
+#define IDD_VIEW_PYRAMID                156
+#define IDD_VIEW_PYRAMID3D              157
+#define IDD_VIEW_FASTLINE               158
+#define IDD_VIEW_SIDEBYDIDESTACKEDBAR   159
+#define IDD_VIEW_STACKEDSPLINEAREA      160
+#define IDC_CHECK_100                   161
+#define IDD_VIEW_GANTT                  161
+#define IDD_VIEW_RADARPOINT             162
+#define IDD_VIEW_RADARLINE              163
+#define IDD_VIEW_RADARAREA              164
+#define IDD_VIEW_RADARSPLINE            165
+#define IDD_VIEW_LOGARITHMIC            166
+#define IDD_VIEW_DATETIME               167
+#define IDC_CHECK_SHOWLABELS            1001
+#define IDC_CHECK_SHOWMARKERS           1002
+#define IDC_CHECK_REVERTED              1002
+#define IDC_CHECK_ROTATED               1002
+#define IDC_CHECK_3DSTYLE               1002
+#define IDC_CHARTCONTROL                1003
+#define IDC_STATIC_LABELS               1004
+#define IDC_CHECK_SHOWLABELS2           1005
+#define IDC_CHECK_INVERTEDSTEP          1005
+#define IDC_CHECK_SIDEBYSIDE            1005
+#define IDC_CHECK_AXISX                 1005
+#define IDC_CHECK_ALLOWZOOM             1005
+#define IDC_CHECK_EQUALHEIGHT           1005
+#define IDC_COMBO_TRANSPARENCY          1006
+#define IDC_CHECK_AXISY                 1006
+#define IDC_CHECK_ALLOWSCROLL           1006
+#define IDC_EDIT_MIN                    1007
+#define IDC_EDIT_MAX                    1008
+#define IDC_COMBO_LABEL_POSITION        1010
+#define IDC_COMBO_EXPLODED_POINTS       1011
+#define IDC_COMBO_MEASUREUNITS          1011
+#define IDC_EDIT_HOLE_RADIUS            1012
+#define IDC_COMBO_POINTDISTANCE         1012
+#define IDC_COMBO_MARKER_SIZE           1014
+#define IDC_COMBO_MARKER_TYPE           1015
+#define IDC_COMBO_ALIGNMENT             1016
+#define IDC_COMBO_LABEL_ANGLE           1017
+#define IDC_EDIT_TEXT                   1018
+#define IDC_COMBO_DOCK                  1019
+#define IDC_COMBO_DIRECTION             1020
+#define IDC_CHECK_VISIBLEX              1021
+#define IDC_CHECK_VISIBLEY              1022
+#define IDC_CHECK_THICKSTYLE            1024
+#define IDC_COMBO_HEIGHTTOWIDTH         1025
+#define IDC_COMBO_SERIES                1025
+#define IDC_COMBO_POINTS                1026
+#define IDC_COMBO_INTERVAL              1027
+#define IDC_INTERVAL                    1029
+#define IDC_CHECK_INTERLACED            1030
+#define IDC_CHECK_ANTIALIASED           1031
+#define IDC_BUTTON_ADD                  1032
+#define IDC_RADIO_GROUP                 1033
+#define IDC_RADIO_QUATER                1034
+#define IDC_COMBO_LABEL_FORMAT          1035
+#define ID_PALETTE_PALETTE              5000
+#define ID_APPEARANCE_APPEARANCE        5050
+#define IDR_BUILDERFILE                 31006
+#define IDR_PANE_EXPLORER               57608
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        162
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1036
+#define _APS_NEXT_SYMED_VALUE           104
+#endif
+#endif
