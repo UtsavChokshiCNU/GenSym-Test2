13a2475acaedaf5ec22839ea3c1a13c60b33bba3
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:48:37 +0400

    Commiting all

---
+++ src/xtp/Samples/Controls/MarqueeProgress/resource.h
@@ -0,0 +1,68 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by MarqueeProgress.rc
+//
+// This file is a part of the XTREME TOOLKIT PRO MFC class library.
+// (c)1998-2011 Codejock Software, All Rights Reserved.
+//
+// THIS SOURCE FILE IS THE PROPERTY OF CODEJOCK SOFTWARE AND IS NOT TO BE
+// RE-DISTRIBUTED BY ANY MEANS WHATSOEVER WITHOUT THE EXPRESSED WRITTEN
+// CONSENT OF CODEJOCK SOFTWARE.
+//
+// THIS SOURCE CODE CAN ONLY BE USED UNDER THE TERMS AND CONDITIONS OUTLINED
+// IN THE XTREME TOOLKIT PRO LICENSE AGREEMENT. CODEJOCK SOFTWARE GRANTS TO
+// YOU (ONE SOFTWARE DEVELOPER) THE LIMITED RIGHT TO USE THIS SOFTWARE ON A
+// SINGLE COMPUTER.
+//
+// CONTACT INFORMATION:
+// support@codejock.com
+// http://www.codejock.com
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#define IDR_MANIFEST                    1
+#define IDM_ABOUTBOX                    0x0010
+#define IDS_ABOUTBOX                    101
+#define IDD_MARQUEEPROGRESS_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDC_PROG_HORZ                   1000
+#define IDC_PROG_VERT                   1001
+#define IDC_CHK_MARQUEE                 1002
+#define IDC_EDIT_INTERVAL               1003
+#define IDC_EDIT_GAP                    1004
+#define IDC_EDIT_CX                     1005
+#define IDC_EDIT_CHUNK1                 1007
+#define IDC_EDIT_CHUNK2                 1008
+#define IDC_EDIT_CHUNK3                 1009
+#define IDC_EDIT_CHUNK4                 1010
+#define IDC_EDIT_CHUNK5                 1011
+#define IDC_SPIN_INTERVAL               1012
+#define IDC_SPIN_GAP                    1013
+#define IDC_SPIN_CX                     1014
+#define IDC_SPIN_CHUNK1                 1016
+#define IDC_SPIN_CHUNK2                 1017
+#define IDC_SPIN_CHUNK3                 1018
+#define IDC_SPIN_CHUNK4                 1019
+#define IDC_SPIN_CHUNK5                 1020
+#define IDC_BTN_UPDATE                  1021
+#define IDC_STATIC_INTERVAL             1023
+#define IDC_STATIC_GAP                  1024
+#define IDC_STATIC_CX                   1025
+#define IDC_STATIC_CHUNK1               1026
+#define IDC_STATIC_CHUNK2               1027
+#define IDC_STATIC_CHUNK3               1028
+#define IDC_STATIC_CHUNK4               1029
+#define IDC_STATIC_CHUNK5               1030
+#define IDC_STATIC_SETTINGS             1031
+#define IDC_STATIC_TRANSPARENCY         1032
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1033
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
