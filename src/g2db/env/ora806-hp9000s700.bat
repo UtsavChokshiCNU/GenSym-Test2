f81025d7344758dd637ffd2051fe70269f438140
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:06 +0400

    Commiting all

---
+++ src/g2db/env/ora806-hp9000s700.bat
@@ -0,0 +1,3 @@
+setenv ORACLE_HOME     /gensym/bridges/hp9000s700/dbs/oracle/806
+setenv LD_LIBRARY_PATH /gensym/bridges/hp9000s700/dbs/oracle/806/lib
+setenv SHLIB_PATH      /gensym/bridges/hp9000s700/dbs/oracle/806/lib
