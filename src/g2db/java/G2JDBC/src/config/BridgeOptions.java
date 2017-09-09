d79c45280dc4613dc613dc62ee2f8c55d1fa7bf9
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:15 +0400

    Commiting all

---
+++ src/g2db/java/G2JDBC/src/config/BridgeOptions.java
@@ -0,0 +1,19 @@
+package config;
+
+/**
+ * Names of the bridge command-line options
+ * @author SoftServe
+ *
+ */
+public final class BridgeOptions {
+
+	public static final String PORT = "-port";
+	public static final String MAX_ROWS = "-maxrows";
+	public static final String MAX_COLUMNS = "-maxcols";
+	public static final String CHARSET = "-charset";
+	public static final String SHUTDOWN_ON_DISCONNECT = "-shutdown_on_disconnect";
+	public static final String HELP = "-help";
+	public static final String DRIVER = "-driver";
+	public static final String DUMP_METADATA = "-dmd";
+	
+}
