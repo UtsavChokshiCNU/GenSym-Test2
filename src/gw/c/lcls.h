c0382116e082da45a1af5b03f7818f105ee92f8f
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:09:23 +0400

    Commiting all

---
+++ src/gw/c/lcls.h
@@ -0,0 +1,29 @@
+/* lcls.h -- Header File for lcls.c
+ * Copyright (C) 1986-2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+/* Symbol Definitions */
+
+extern Object Pclos;
+extern Object Pclos_run;
+extern Object Pclosx;
+extern Object Pcl;
+extern Object Pcl_user;
+extern Object Pkeyword;
+extern Object Ptrun;
+extern Object Ptx;
+
+/* function declarations */
+
+#ifdef USE_PROTOTYPES
+
+extern Object g2000_get_code_page_information(void);
+
+#else
+
+extern Object g2000_get_code_page_information();
+
+#endif
+
+/* variables/constants */
