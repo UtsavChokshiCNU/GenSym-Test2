ab656a204976d3af75e36b749dfda867a5ac3698
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:17 +0400

    Commiting all

---
+++ src/g2db/java/G2JDBC/src/type/ReturnUpdateControlValues.java
@@ -0,0 +1,19 @@
+package type;
+
+/**
+ * Holds Return control values, as defined in the g2-database KB
+ * @author SoftServe
+ *
+ */
+public class ReturnUpdateControlValues {
+	
+	public static final int LIST = 1;
+	public static final int ARRAY = 2;
+	
+	public static final int RELPACE = 1;
+	public static final int APPEND = 2;
+	
+	private ReturnUpdateControlValues() {
+		
+	}
+}
