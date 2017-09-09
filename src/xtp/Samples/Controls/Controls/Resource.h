21a864d07af3ee124f461e65218057592de9aea5
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:48:27 +0400

    Commiting all

---
+++ src/xtp/Samples/Controls/Controls/Resource.h
@@ -0,0 +1,114 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Controls.rc
+//
+#define IDR_MANIFEST                    1
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CONTROLS_DIALOG             102
+#define IDD_PAGE_BUTTONS                102
+#define IDD_PAGE_MARKUPBUTTONS          103
+#define IDR_MAINFRAME                   128
+#define IDD_PAGE_EDIT                   129
+#define IDB_OK                          130
+#define IDD_PAGE_COMBOBOX               130
+#define IDD_PAGE_SCROLLBAR              132
+#define IDD_PAGE_PROGRESSBAR            133
+#define IDR_MENU_DROPDOWN               139
+#define IDR_MENU_ALIGNMENT              140
+#define IDB_IMAGELIST                   141
+#define IDC_EDIT_SINGLELINE             1000
+#define IDC_EDIT_MULTILINE              1001
+#define IDC_EDIT_MASKED                 1002
+#define IDC_EDIT_PATTERN                1003
+#define IDC_EDIT2                       1004
+#define IDC_EDIT_SPIN                   1004
+#define IDC_EDIT3                       1005
+#define IDC_EDIT_BUTTON                 1005
+#define IDC_SPIN1                       1006
+#define IDC_SPIN                        1006
+#define IDC_GBOX_REGULAR                1007
+#define IDC_BUTTON1                     1007
+#define IDC_BUTTON                      1007
+#define IDC_BUTTON_PAUSE                1007
+#define IDC_BUTTON_SETTINGS             1008
+#define IDC_BUTTON_STEP                 1008
+#define IDC_BUTTON_FIND                 1009
+#define IDC_CHECK_READONLY              1009
+#define IDC_COMBO_DROPDOWN              1009
+#define IDC_GBOX_TWOROWS                1010
+#define IDC_CHECK_PASSWORD              1010
+#define IDC_COMBO_DROPDOWNLIST          1010
+#define IDC_BUTTON_HELP                 1011
+#define IDC_COMBO2                      1011
+#define IDC_BUTTON_RUN                  1012
+#define IDC_SCROLLBAR1                  1012
+#define IDC_COMBO_STYLE                 1013
+#define IDC_SCROLLBAR2                  1013
+#define IDC_PROGRESS1                   1013
+#define IDC_SCROLLBAR3                  1014
+#define IDC_PROGRESS2                   1014
+#define IDC_SCROLLBAR4                  1015
+#define IDC_PROGRESS3                   1015
+#define IDC_SCROLLBAR5                  1016
+#define IDC_PROGRESS4                   1016
+#define IDC_SCROLLBAR6                  1017
+#define IDC_PROGRESS5                   1017
+#define IDC_SCROLLBAR7                  1018
+#define IDC_PROGRESS6                   1018
+#define IDC_PROGRESS7                   1019
+#define IDC_GBOX_CHECKBOXES             1030
+#define IDC_CHECK_STATE1                1031
+#define IDC_CHECK_STATE2                1032
+#define IDC_GBOX_RADIO                  1033
+#define IDC_RADIO_OPTION1               1034
+#define IDC_RADIO_OPTION2               1035
+#define IDC_GBOX_JUSTIFICATION          1036
+#define IDC_STATIC_TEXTALIGNEMNT        1037
+#define IDC_COMBO_IMAGEALIGNMENT        1038
+#define IDC_STATIC_IMAGEALIGNMENT       1039
+#define IDC_COMBO_TEXTALIGNMENT         1040
+#define IDC_STATIC_RELATION             1041
+#define IDC_COMBO_RELATION              1042
+#define IDC_GBOX_THEME                  1050
+#define IDC_RADIO_CLASSIC               1051
+#define IDC_RADIO_FLAT                  1052
+#define IDC_RADIO_ULTRAFLAT             1053
+#define IDC_RADIO_OFFICE2000            1054
+#define IDC_RADIO_OFFICEXP              1055
+#define IDC_RADIO_OFFICE2003            1056
+#define IDC_RADIO_OFFICE2007            1057
+#define IDC_CHECK_WINXP_THEMES          1058
+#define IDC_CHECK_FLATSTYLE             1059
+#define IDC_GBOX_OPTIONS                1060
+#define IDC_CHECK_ENABLED               1061
+#define IDC_CHECK_FOCUS                 1062
+#define IDC_CHECK_AUTOCOMPLETE          1062
+#define IDC_CHECK_TOGGLE                1063
+#define IDC_CHECK_ALPHA                 1064
+#define IDB_ALPHA_SETTINGS              1065
+#define IDB_ALPHA_FIND                  1066
+#define IDB_ALPHA_HELP                  1067
+#define IDB_ALPHA_RUN                   1068
+#define IDC_CHECK_MARKUP                1070
+#define IDC_RADIO_MARKUP                1071
+#define IDC_BUTTON_MARKUP               1072
+#define IDC_BUTTON_FIREWALL             1073
+#define IDC_CHECK_DELETEINBOX           1074
+#define IDC_CHECK_DELETEOUTBOX          1075
+#define IDC_RADIO_WITHLINK              1076
+#define ID_DROPDOWNMENU_OPTIONITEM1     32771
+#define ID_DROPDOWNMENU_OPTIONITEM2     32772
+#define ID_DROPDOWNMENU_OPTIONITEM3     32773
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
