5b51562f263dec7d2fc461b0c0b7a9746305bea9
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:35:11 +0400

    Commiting all

---
+++ src/twng/tests/TestDynamicTabDlg.h
@@ -0,0 +1,28 @@
+#ifndef _TEST_DYNAMIC_TAB_DLG_H_
+#define _TEST_DYNAMIC_TAB_DLG_H_
+
+#include "stdafx.h"
+#include "DynamicTabsDlg.h"
+#include "../ext/c/dyndlgs.h"
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+class CG2TabWidget;
+
+class TestDynamicTabDlg : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE(TestDynamicTabDlg);
+    CPPUNIT_TEST(testTab);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    virtual void setup();
+    virtual void tearDown();
+
+    void testTab();
+
+private:
+	CWnd* pParent;
+	CG2TabWidget *pTab;
+};
+#endif
