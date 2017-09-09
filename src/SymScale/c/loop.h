d9e57c7606ef2c1cfc319f3b3666ac3d671f3a89
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:39:33 +0400

    Commit-1

---
+++ src/SymScale/c/loop.h
@@ -0,0 +1,17 @@
+/* loop.h -- Header File for loop.c
+ * Copyright (C) 1986-2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+/* Symbol Definitions */
+
+extern Object Pclos;
+extern Object Pclos_run;
+extern Object Pclosx;
+extern Object Pcl;
+extern Object Pcl_user;
+extern Object Pkeyword;
+extern Object Ptrun;
+extern Object Ptx;
+
+/* variables/constants */
