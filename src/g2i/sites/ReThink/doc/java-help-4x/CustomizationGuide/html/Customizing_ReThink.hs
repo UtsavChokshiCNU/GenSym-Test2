d99df2ba1f99bb6999b29e30183ada0c0ea442b4
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:07:24 +0400

    Commiting all

---
+++ src/g2i/sites/ReThink/doc/java-help-4x/CustomizationGuide/html/Customizing_ReThink.hs
@@ -0,0 +1,32 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<helpset version="1.0">
+  <title>Customizing ReThink</title>
+  <maps>
+    <homeID>preface_htm_25184</homeID>
+    <mapref location="Customizing_ReThink.jhm" />
+  </maps>
+  <view>
+    <name>TOC</name>
+    <label>Customizing ReThink</label>
+    <type>javax.help.TOCView</type>
+    <data>bookltc.xml</data>
+  </view>
+  <view>
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>bookix.xml</data>
+  </view>
+  <view>
+    <name>Search</name>
+    <label>Search</label>
+    <type>javax.help.SearchView</type>
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">
+      JavaHelpSearch
+    </data>
+  </view>
+</helpset>
