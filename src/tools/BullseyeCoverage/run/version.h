f7ffcadd8a078e03705d3bdda8d3e85c3dd36414
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:30:52 +0400

    Commiting all

---
+++ src/tools/BullseyeCoverage/run/version.h
@@ -0,0 +1,9 @@
+/* $Id: version.h.in 9464 2008-11-19 01:53:21Z Steve Cornett $
+ * Copyright (c) Bullseye Testing Technology
+ */
+
+#define VERSION_normal "7.13.38"
+#define VERSION_number "713038"
+#define VERSION_title "BullseyeCoverage 7.13.38"
+#define VERSION_copyright "Copyright (c) Bullseye Testing Technology 1990-2010"
+#define _BullseyeCoverage 713038
