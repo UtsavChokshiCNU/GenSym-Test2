1f8eb1493207d7b0bcab31a5d406922354db44d8
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:19:56 +0400

    Commiting all

---
+++ src/lic/Instanttemps/eulalist.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+public partial class eulalist : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Cookies["val"] == null)
+            Response.Redirect("admin.aspx");
+    }
+}
