cc68c7e7fb0ce73b67f38c9f167a10b3c34d93ec
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:39:37 +0400

    Commit-1

---
+++ src/SymScale/c/symtab.c
@@ -0,0 +1,23 @@
+/* symtab.c -- Lisp-to-C Translator Symbol Definitions
+ * Copyright (C) 1986-2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+#include "user.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
