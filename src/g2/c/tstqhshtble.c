a9bfb4ea04a582b0dd95654297a27c984e6570d1
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:01:20 +0400

    Commiting all

---
+++ src/g2/c/tstqhshtble.c
@@ -0,0 +1,14 @@
+/* tstqhshtble.c
+ * Input file:  test-eq-hash-table.lisp
+ * Copyright (c) 1986 - 2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+#include "user.h"
+#include "tstqhshtble.h"
+
+
+void test_eq_hash_table_INIT()
+{
+    x_note_fn_call(256,0);
+    SET_PACKAGE("AB");
+}
