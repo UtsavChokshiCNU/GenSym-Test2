35c8e3af981085c11a73d579b9d1128dadab0359
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:09:18 +0400

    Commiting all

---
+++ src/gw/c/gm_main.c
@@ -0,0 +1,11 @@
+/*******************************************************************************
+:
+:  gm_main.c  --  The main for gm, aka G2 WebMiner
+: Here is the key to the C level variation between the two products.
+:
+:.............................................................................*/
+
+/*  374049449275592253  @@@@Mon Mar 24 16:57:48 EST 1997@@@@  */
+
+#define I_AM_GM
+#include "gw_main.c"
