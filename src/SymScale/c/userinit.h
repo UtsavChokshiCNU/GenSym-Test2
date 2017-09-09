23cf6a36d9c24935b6fbb776ab2eea87b6665bbd
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:39:38 +0400

    Commit-1

---
+++ src/SymScale/c/userinit.h
@@ -0,0 +1,5 @@
+/* This is one of a few dummy include files that exist merely so that the
+ * corresponding .c file will have a .h file.  Though not necessary, the file
+ * provides greater regularity of structure, thereby simplifying the
+ * makefile, allowing freer use of default rules, etc.
+ */
