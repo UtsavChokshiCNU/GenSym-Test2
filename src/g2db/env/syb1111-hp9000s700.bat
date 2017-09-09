70c52247bbb983da3a30c3e94c720d7b644382c2
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:09 +0400

    Commiting all

---
+++ src/g2db/env/syb1111-hp9000s700.bat
@@ -0,0 +1,3 @@
+setenv SYBASE          /gensym/bridges/hp9000s700/dbs/sybase/1111
+setenv LD_LIBRARY_PATH /gensym/bridges/hp9000s700/dbs/sybase/1111/lib
+setenv SHLIB_PATH      /gensym/bridges/hp9000s700/dbs/sybase/1111/lib
