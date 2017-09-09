f8690681ab9a695a0954480f1fbfadbd76e94019
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:09:21 +0400

    Commiting all

---
+++ src/gw/c/htmlgn.h
@@ -0,0 +1,18 @@
+/* htmlgn.h -- Header File for htmlgn.c
+ * Copyright (c) 1986 - 2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+/* Symbol Definitions */
+
+extern Object Pclos;
+extern Object Pclos_run;
+extern Object Pclosx;
+extern Object Pcl;
+extern Object Pcl_user;
+extern Object Pkeyword;
+extern Object Ptrun;
+extern Object Ptx;
+
+/* variables/constants */
+extern Object G2int_current_html_insert_mark;
