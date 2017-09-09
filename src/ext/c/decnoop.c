3d10f202177966be920f057364f96b81e5c87b16
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:58:36 +0400

    Commiting all

---
+++ src/ext/c/decnoop.c
@@ -0,0 +1,27 @@
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ +
+ + Module:      decnoop.c
+ +
+ + Copyright (C) 1986-2017 Gensym Corporation.
+ +            All Rights Reserved.
+ +
+ + Author(s):    Brian L. Matthews and Ong, Peng Tsin
+ +               John Hodgkinson, Nick Caruso
+ +
+ + Description: This module is obsolete, because DECNet is no longer supported.
+ + 
+ + Key:
+ +
+ + Dependencies:
+ +      This file has no external dependencies.
+ +
+ + Notes:
+ + Modifications:
+ +   usage: All modifications to this file should be listed here, with most
+ +          recent first. In general, modification comments should merely
+ +          provide a reference to a detailed modification comment in the
+ +          appropriate section/function header.
+ +
+ +   10/8/05, jv - we no longer support DECNet
+ +
+ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
