e2ac5b894e42151a41fd12d2f83055de012a0be7
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:00:55 +0400

    Commiting all

---
+++ src/g2/c/gbmapr.h
@@ -0,0 +1,30 @@
+/* gbmapr.h -- Header File for gbmapr.c
+ * Copyright (c) 1986 - 2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+/* Symbol Definitions */
+
+extern Object Pclos;
+extern Object Pclos_run;
+extern Object Pclosx;
+extern Object Pcl;
+extern Object Pcl_user;
+extern Object Pkeyword;
+extern Object Ptrun;
+extern Object Ptx;
+
+/* function declarations */
+
+#ifdef USE_PROTOTYPES
+
+extern Object regenerate_optimized_constant(Object);
+
+#else
+
+extern Object regenerate_optimized_constant();
+
+#endif
+
+/* variables/constants */
+extern Object Unicode_to_gb_2312_map;
