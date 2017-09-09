39fc2d051f111a93e7aa62590afcf67d124f1513
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:47:52 +0400

    Commiting all

---
+++ src/xtp/Samples/Chart/ChartDialog/Resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ChartDialog.rc
+//
+#define IDR_MANIFEST                    1
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CHARTDIALOG_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDC_CHARTCONTROL                1000
+#define IDC_COMBO_STYLE                 1001
+#define IDC_COMBO_APPEARANCE            1002
+#define IDC_COMBO_PALETTE               1003
+#define IDC_CHECK_LEGEND                1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
