4f11956875fb170f561d38146836f67917392b68
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:32:39 +0400

    Commiting all

---
+++ src/twng/c/DynamicBaseDlg.h
@@ -0,0 +1,27 @@
+//DynamicBaseDlg.h:header file
+//
+#pragma once
+
+#include "resource.h"
+
+class CDynamicBaseDlg : public CDialog
+{
+public:
+  DECLARE_DYNCREATE(CDynamicBaseDlg)
+  CDynamicBaseDlg(CWnd* pParent = NULL);
+  virtual BOOL PreTranslateMessage(MSG* pMsg);
+
+  CToolTipCtrl m_ToolTipControl;
+  enum {IDD = IDD_DYNAMIC_BASE_DLG};
+
+protected:
+  virtual void PostNcDestroy();
+  LRESULT OnDurationHelp(WPARAM, LPARAM);
+  DECLARE_MESSAGE_MAP()
+  afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+  afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+  afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
+private:
+  void PrintMe();
+  HBITMAP GetDialogImage();
+};
