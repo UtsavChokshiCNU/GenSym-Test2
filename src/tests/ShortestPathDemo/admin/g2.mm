6267c909e5d3c2082f55f0d10b9f6ba3544d4200
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:29:47 +0400

    Commiting all

---
+++ src/tests/ShortestPathDemo/admin/g2.mm
@@ -0,0 +1,30 @@
+// G2 utility kbs
+uilroot C:\G2\src\kbs\uilroot.kb
+sys-mod C:\G2\src\kbs\sys-mod.kb
+gfr C:\G2\src\kbs\gfr.kb
+gxl C:\G2\src\kbs\gxl.kb
+gms C:\G2\src\kbs\gms.kb
+uil C:\G2\src\kbs\uil.kb
+g2uifile C:\G2\src\kbs\g2uifile.kb
+g2uimenu C:\G2\src\kbs\g2uimenu.kb
+g2web C:\G2\src\kbs\g2web.kb
+
+// G2i
+gdu C:\G2\src\g2i\kbs\gdu.kb
+gerr C:\G2\src\g2i\kbs\gerr.kb
+grtl C:\G2\src\g2i\kbs\grtl.kb
+grpe C:\G2\src\g2i\kbs\grpe.kb
+
+// Bridges
+javalink C:\G2\src\javalink\kbs\javalink.kb
+g2com C:\G2\src\kbs\g2com.kb
+g2-database C:\G2\src\g2db\kbs\g2-database.kb
+
+// G2 Protools kbs
+protools C:\G2\src\kbs\protools.kb
+ptroot C:\G2\src\kbs\ptroot.kb
+
+// Gensym Consulting kbs
+vtt-root C:\G2\src\tests\ShortestPathDemo\kbs\vtt-root.kb
+vtt-tools C:\G2\src\tests\ShortestPathDemo\kbs\vtt-tools.kb
+
