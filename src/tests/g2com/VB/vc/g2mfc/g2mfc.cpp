09d57fe964a253500ec3915e85dba0fea3962312
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:28:50 +0400

    Commiting all

---
+++ src/tests/g2com/VB/vc/g2mfc/g2mfc.cpp
@@ -0,0 +1,74 @@
+// g2mfc.cpp : Defines the class behaviors for the application.
+//
+
+#include "stdafx.h"
+#include "g2mfc.h"
+#include "g2mfcDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CG2mfcApp
+
+BEGIN_MESSAGE_MAP(CG2mfcApp, CWinApp)
+	//{{AFX_MSG_MAP(CG2mfcApp)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+		//    DO NOT EDIT what you see in these blocks of generated code!
+	//}}AFX_MSG
+	ON_COMMAND(ID_HELP, CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CG2mfcApp construction
+
+CG2mfcApp::CG2mfcApp()
+{
+	// TODO: add construction code here,
+	// Place all significant initialization in InitInstance
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// The one and only CG2mfcApp object
+
+CG2mfcApp theApp;
+
+/////////////////////////////////////////////////////////////////////////////
+// CG2mfcApp initialization
+
+BOOL CG2mfcApp::InitInstance()
+{
+	AfxEnableControlContainer();
+
+	// Standard initialization
+	// If you are not using these features and wish to reduce the size
+	//  of your final executable, you should remove from the following
+	//  the specific initialization routines you do not need.
+
+#ifdef _AFXDLL
+	Enable3dControls();			// Call this when using MFC in a shared DLL
+#else
+	Enable3dControlsStatic();	// Call this when linking to MFC statically
+#endif
+
+	CG2mfcDlg dlg;
+	m_pMainWnd = &dlg;
+	int nResponse = dlg.DoModal();
+	if (nResponse == IDOK)
+	{
+		// TODO: Place code here to handle when the dialog is
+		//  dismissed with OK
+	}
+	else if (nResponse == IDCANCEL)
+	{
+		// TODO: Place code here to handle when the dialog is
+		//  dismissed with Cancel
+	}
+
+	// Since the dialog has been closed, return FALSE so that we exit the
+	//  application, rather than start the application's message pump.
+	return FALSE;
+}
