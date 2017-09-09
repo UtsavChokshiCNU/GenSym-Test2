f54d90d4176568c51dff4c31ccbe6baabddc14b7
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:09 +0400

    Commiting all

---
+++ src/g2db/env/syb1150-intelnt.bat
@@ -0,0 +1,4 @@
+PATH=c:\sp\bat;c:\sp\util;C:\WINNT\system32;C:\WINNT;C:\WINNT\System32\WBEM;k:\gensym\spare\bridges\intelnt\dbs\sybase\1150\bin;k:\gensym\spare\bridges\intelnt\dbs\sybase\1150\dll
+set SYBASE=k:\gensym\spare\bridges\intelnt\dbs\sybase\1150
+call VCVARS32.bat
+
