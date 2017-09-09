7e2be990943ac52881e8abb812d2c1da5c092ab7
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:07:38 +0400

    Commiting all

---
+++ src/g2opc/cpp/resource.h
@@ -0,0 +1,62 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by opclink.rc
+//
+#define IDC_MYICON                      2
+#define ID_FIND_SERVERS                 3
+#define ID_CLOSE_DLG                    4
+#define IDD_CTRLPANEL                   101
+#define IDD_ABOUTBOX                    103
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDS_HELLO                       106
+#define IDI_G2OPC                       107
+#define IDI_SMALL                       108
+#define IDC_G2OPC                       109
+#define IDD_QT                          112
+#define IDD_PROJQ                       113
+#define IDD_SERVERLIST                  114
+#define IDB_BITMAP1                     131
+#define IDD_DIALOG1                     133
+#define IDD_DESIGN                      135
+#define IDC_TRACE_LEVEL                 201
+#define IDC_LOG_TO_G2                   202
+#define IDC_LOG_TO_SCREEN               203
+#define IDC_ARCHIVE_LOG                 204
+#define IDC_LOGGING_ENABLED             205
+#define IDC_QUALITY_FILTER              206
+#define IDC_CHK_SRVR_STAT               207
+#define IDC_SRVR_CHK_FREQ               208
+#define IDC_USE_G2_TIMESTAMP            209
+#define IDC_CHECK1                      1000
+#define IDC_OPC1                        1000
+#define IDC_BUTTON1                     1001
+#define IDC_CHECK2                      1001
+#define IDC_OPC2                        1001
+#define IDC_RADIO1                      1002
+#define IDC_TL0                         1002
+#define IDC_CHECK3                      1002
+#define IDC_OPC3                        1002
+#define IDC_ANIMATE1                    1003
+#define IDC_TL1                         1003
+#define IDC_SPIN1                       1004
+#define IDC_TL2                         1004
+#define IDC_COMBO1                      1005
+#define IDC_TL3                         1005
+#define IDC_TL4                         1006
+#define IDC_SERVERS                     1009
+#define ID_CONTROLPANEL                 32771
+#define ID_AUTODISCOVERY                32772
+#define ID_APP_EXIT                     0xE141
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        141
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           115
+#endif
+#endif
