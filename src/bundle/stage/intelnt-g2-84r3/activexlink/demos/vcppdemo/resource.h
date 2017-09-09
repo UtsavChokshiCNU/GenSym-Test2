cc5b99b2ecb7778d4d849817a3e485fac1d727df
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:43:47 +0400

    Commiting all

---
+++ src/bundle/stage/intelnt-g2-84r3/activexlink/demos/vcppdemo/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by VCppDemo.rc
+//
+#define IDBTN_DISCON                    2
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_VCppDemo_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     129
+#define IDC_BUTTON1                     1000
+#define IDC_POST_BUTTON                 1000
+#define ID_G2GATEWAY                    1002
+#define IDC_MSG                         1002
+#define IDC_BUTTON_CALLG2               1003
+#define IDC_CALL_ITEM                   1004
+#define IDC_CALL_ITEM_RES               1005
+#define IDC_CYCLE_BUTTON                1006
+#define IDC_G2LOCATION                  1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
