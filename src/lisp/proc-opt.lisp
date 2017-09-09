7a5af6322426b1ef3288dea3033c3b36714cc202
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:20:31 +0400

    Commiting all

---
+++ src/lisp/proc-opt.lisp
@@ -0,0 +1,15 @@
+;; -*- Mode:Lisp; Package: AB; Base: 10; Syntax: Common-lisp -*-
+(in-package "AB")
+
+;;;; Module PROC-OPT
+
+;;; Copyright (c) 2009 Versata Inc.
+;;; All rights reserved.
+
+#| COMPLETELY DISABLED IN GENSYMCID-1324 BY CHUN TIAN on 5 February 2015
+
+(defun optimize-intermediate-code (proc-instructions)
+  ...
+  )
+
+|#
