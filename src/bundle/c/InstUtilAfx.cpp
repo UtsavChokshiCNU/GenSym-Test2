d5f8e86c73e6b5c911dada2bd3e8ebb24d097841
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:43:22 +0400

    Commiting all

---
+++ src/bundle/c/InstUtilAfx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	Gensym.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "InstUtilAfx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
