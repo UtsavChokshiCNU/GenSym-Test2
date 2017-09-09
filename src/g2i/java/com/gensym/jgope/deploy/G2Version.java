ebc5e42c06ed7b97a4214d097b0e7cb787055afd
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:05:05 +0400

    Commiting all

---
+++ src/g2i/java/com/gensym/jgope/deploy/G2Version.java
@@ -0,0 +1,10 @@
+package com.gensym.jgope.deploy;
+
+public class G2Version {
+
+  public static final String VERSION_INFORMATION = "Version 5.2 Alpha Rev. 0";
+  public static final String BUILD_INFORMATION = "5020000";
+  public static final String BUILD_DATE = "30 Oct 2013  9:06:34 p.m.";
+  public static final String COPYRIGHT_INFORMATION = "Gensym Corporation 2002-2013";
+ 
+}
