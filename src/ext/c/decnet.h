88763d5f28a68dff9bb85862db1b835ca7fc6949
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:58:36 +0400

    Commiting all

---
+++ src/ext/c/decnet.h
@@ -0,0 +1,10 @@
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ %
+ % Section:      DEFINES
+ %
+ % Description:  Module specific macros defined in this section. 
+ %
+ % Notes:
+ %    10/8/05, jv - we no longer support DECNet
+ % Modifications:
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
