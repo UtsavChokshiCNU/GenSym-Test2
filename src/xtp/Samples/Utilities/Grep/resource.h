e9c36e66d79b4980e7dc9cfa92def82e176f1df9
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:50:13 +0400

    Commiting all

---
+++ src/xtp/Samples/Utilities/Grep/resource.h
@@ -0,0 +1,72 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Grep.rc
+//
+#define IDR_MANIFEST                    1
+#define IDR_MAINFRAME                   128
+#define IDR_GREPTYPE                    129
+#define IDB_BITMAP_ICONS                130
+#define IDD_DIALOG_OPTIONS              130
+#define IDR_SEARCH_POPUP                131
+#define IDB_TOOLBAR_ALPHA               132
+#define IDD_DIALOG_SETTINGS             132
+#define IDC_COMBO_FIND                  1001
+#define IDC_BUTTON_FIND_EXPAND          1002
+#define IDC_COMBO_REPLACE               1003
+#define IDC_BUTTON_REPLACE_EXPAND       1004
+#define IDC_COMBO_PATH                  1005
+#define IDC_BUTTON_PATH_EXPAND          1006
+#define IDC_CHECK_INCLUDE_SUBFOLDERS    1007
+#define IDC_CHECK_MATCH_CASE            1008
+#define IDC_CHECK_MATCH_WHOLE_WORDS     1009
+#define IDC_CHECK_REGULAR               1010
+#define IDC_CHECK_HIDDEN                1011
+#define IDC_COMBO_TYPE                  1012
+#define IDC_GROUP_FINDOPTIONS           1013
+#define IDC_BUTTON_FINDALL              1014
+#define IDC_BUTTON_REPLACEALL           1015
+#define IDC_STATIC_FIND                 1016
+#define IDC_STATIC_REPLACE              1017
+#define IDC_STATIC_PATH                 1018
+#define IDC_STATIC_LOOK                 1019
+#define IDC_RADIO_EDITOR                1020
+#define IDC_STATIC_BATCH                1020
+#define IDC_EDIT_EDITOR                 1021
+#define IDC_RADIO_ASSOCIATED            1022
+#define IDC_BUTTON_BATCH_EXPAND         1022
+#define IDC_EDIT_BATCH                  1024
+#define IDR_PANE_OPTIONS                4000
+#define IDR_PANE_PROPERTIES             4001
+#define ID_TOOLBAR_REPLACEINFILES       4002
+#define ID_TOOLBAR_BATCH                4003
+#define ID_SEARCH_ANYSINGLECHARACTER    32771
+#define ID_SEARCH_REGEXP1               32772
+#define ID_SEARCH_REGEXP2               32773
+#define ID_SEARCH_REGEXP3               32774
+#define ID_SEARCH_BEGININGOFLINE        32775
+#define ID_SEARCH_REGEXP4               32776
+#define ID_SEARCH_ENDOFLINE             32777
+#define ID_SEARCH_REGEXP7               32778
+#define ID_SEARCH_REGEXP6               32779
+#define ID_SEARCH_REGEXP5               32780
+#define ID_SEARCH_REGEXP8               32781
+#define ID_REPLACE_FINDWHATTEXT         32782
+#define ID_REPLACE_REGEXP1              32783
+#define ID_REPLACE_REGEXP2              32784
+#define ID_REPLACE_REGEXP3              32785
+#define ID_REPLACE_REGEXP4              32786
+#define ID_REPLACE_REGEXP5              32787
+#define ID_VIEW_OPTIONSPANE             32788
+#define ID_VIEW_SETTINGS                32789
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32790
+#define _APS_NEXT_CONTROL_VALUE         1025
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
