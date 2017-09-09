4295381decb7bfe9cc37de41ef7b45baf3ee5828
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:39:38 +0400

    Commit-1

---
+++ src/SymScale/c/tran.h
@@ -0,0 +1,20 @@
+/* tran.h -- Header File for tran.c
+ * Copyright (C) 1986-2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+/* Symbol Definitions */
+
+extern Object Pclos;
+extern Object Pclos_run;
+extern Object Pclosx;
+extern Object Pcl;
+extern Object Pcl_user;
+extern Object Pkeyword;
+extern Object Ptrun;
+extern Object Ptx;
+
+/* variables/constants */
+DECLARE_VARIABLE_WITH_SYMBOL(Dummy_place_for_chestnut_internals, Qdummy_place_for_chestnut_internals);
+DECLARE_VARIABLE_WITH_SYMBOL(Modules_to_translate_verbosely, Qmodules_to_translate_verbosely);
+extern Object The_standard_mnemonics;
