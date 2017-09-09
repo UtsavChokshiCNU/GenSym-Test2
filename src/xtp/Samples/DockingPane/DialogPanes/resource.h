e85049d8d609f636d0a33bb3e296124259a75741
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:48:57 +0400

    Commiting all

---
+++ src/xtp/Samples/DockingPane/DialogPanes/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by DialogPanes.rc
+//
+#define IDR_MANIFEST                    1
+#define IDM_ABOUTBOX                    0x0010
+#define IDS_ABOUTBOX                    101
+#define IDD_DIALOGPANES_DIALOG          102
+#define IDR_PANE1                       102
+#define IDR_PANE2                       103
+#define IDR_PANE3                       104
+#define IDR_MAINFRAME                   128
+#define IDC_LIST_VIEW                   1000
+#define IDC_LIST_THEMES                 1002
+#define IDC_TXT_THEMES                  1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
