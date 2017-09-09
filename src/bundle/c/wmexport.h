ebb07e49ba6781ec55a6814426d3779a6b11c7f8
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:43:23 +0400

    Commiting all

---
+++ src/bundle/c/wmexport.h
@@ -0,0 +1,5 @@
+extern "C" __declspec(dllexport)  int add_watermark_to_ok_file (char *, char *, char *, char *, char *);
+
+extern "C" __declspec(dllexport)  int make_watermark_file (char *, char *, char *, char *, char *);
+
+extern "C" __declspec(dllexport)  int CheckDllVersion (char *, int, int, char *);
