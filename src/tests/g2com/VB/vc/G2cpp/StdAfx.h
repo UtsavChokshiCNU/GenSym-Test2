98772d8b84056ed0cbfad9e6536c672656386388
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:28:49 +0400

    Commiting all

---
+++ src/tests/g2com/VB/vc/G2cpp/StdAfx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__3E75FFEB_D9F3_4BF8_9149_AF5143E8D925__INCLUDED_)
+#define AFX_STDAFX_H__3E75FFEB_D9F3_4BF8_9149_AF5143E8D925__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__3E75FFEB_D9F3_4BF8_9149_AF5143E8D925__INCLUDED_)
