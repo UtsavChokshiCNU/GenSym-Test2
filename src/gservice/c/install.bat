e0a3facdbf23da26202d45cccab07eee222f204b
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:08:57 +0400

    Commiting all

---
+++ src/gservice/c/install.bat
@@ -0,0 +1,2 @@
+rem Example installation batch file.  Currently not distributed.
+gservice install xyzservice program=c:\rjf\msvcc\oracle\ora60b0\release\ora60b0.exe arguments=22055 logfile=c:\temp\test.log
