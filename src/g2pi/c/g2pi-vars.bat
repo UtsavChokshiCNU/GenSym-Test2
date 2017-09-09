2e2552a05e92053a5157c178e2fa49c5d9b0d804
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:07:40 +0400

    Commiting all

---
+++ src/g2pi/c/g2pi-vars.bat
@@ -0,0 +1,5 @@
+call vcvars32
+echo Add Platform SDK to front of INCLUDE and LIB -fmw 7/27/05
+set APPVER=4.0
+set INCLUDE=C:\Program Files\Microsoft SDK\Include;%INCLUDE%
+set LIB=C:\Program Files\Microsoft SDK\Lib;%LIB%
