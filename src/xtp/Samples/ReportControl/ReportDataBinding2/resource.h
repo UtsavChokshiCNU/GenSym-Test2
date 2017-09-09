2b0c1afcab41a81829b1db62e51c17db6305e599
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:49:15 +0400

    Commiting all

---
+++ src/xtp/Samples/ReportControl/ReportDataBinding2/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ReportDataBinding2.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDS_ABOUTBOX                    101
+#define IDD_REPORTDATABINDING2_DIALOG   102
+#define IDR_MAINFRAME                   128
+#define IDB_HELP                        130
+#define IDB_CJ_LOGO                     131
+#define IDI_ICON1                       132
+#define IDR_POPUP_MENU                  133
+#define IDC_CJ_LINE                     1000
+#define IDC_HELP_BUTTON                 1001
+#define IDC_TABLES_LIST                 1002
+#define IDC_RECORDS_REPORT              1003
+#define IDC_ACTIONS_BUTTON              1004
+#define IDC_DESCR_TEXT                  1005
+#define ID_ADD_RECORDS                  32771
+#define ID_ADD_RECORDS_TO_RECORDSET     32772
+#define ID_DELETE_RECORDS_FROM_RECORDSET 32773
+#define ID_DELETE_RECORDS               32774
+#define IDS_CONNECTION_STRING           57345
+#define IDS_DATABASE_NAME               57346
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
