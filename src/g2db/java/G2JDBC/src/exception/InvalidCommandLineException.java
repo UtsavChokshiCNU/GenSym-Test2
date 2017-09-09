2dbbbbb9582a2beb7a79981611acee899723dc99
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:17 +0400

    Commiting all

---
+++ src/g2db/java/G2JDBC/src/exception/InvalidCommandLineException.java
@@ -0,0 +1,15 @@
+package exception;
+
+/**
+ * This exception will be fired when bridge command line is invalid
+ * @author SoftServe
+ *
+ */
+public class InvalidCommandLineException extends Exception {
+
+	/**
+	 * For serialization purposes
+	 */
+	private static final long serialVersionUID = -8594992090229307083L;
+
+}
