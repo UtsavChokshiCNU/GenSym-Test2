bbf080d86aca46c67f4ca4d3d1c0ee57ba39b831
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:48:43 +0400

    Commiting all

---
+++ src/xtp/Samples/Controls/ShortcutList/ShortcutListBoxView.h
@@ -0,0 +1,78 @@
+// ShortcutListBoxView.h : header file
+//
+// This file is a part of the XTREME TOOLKIT PRO MFC class library.
+// (c)1998-2011 Codejock Software, All Rights Reserved.
+//
+// THIS SOURCE FILE IS THE PROPERTY OF CODEJOCK SOFTWARE AND IS NOT TO BE
+// RE-DISTRIBUTED BY ANY MEANS WHATSOEVER WITHOUT THE EXPRESSED WRITTEN
+// CONSENT OF CODEJOCK SOFTWARE.
+//
+// THIS SOURCE CODE CAN ONLY BE USED UNDER THE TERMS AND CONDITIONS OUTLINED
+// IN THE XTREME TOOLKIT PRO LICENSE AGREEMENT. CODEJOCK SOFTWARE GRANTS TO
+// YOU (ONE SOFTWARE DEVELOPER) THE LIMITED RIGHT TO USE THIS SOFTWARE ON A
+// SINGLE COMPUTER.
+//
+// CONTACT INFORMATION:
+// support@codejock.com
+// http://www.codejock.com
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_SHORTCUTLISTBOXVIEW_H__790EB214_2913_11D3_ABAE_94F400C10000__INCLUDED_)
+#define AFX_SHORTCUTLISTBOXVIEW_H__790EB214_2913_11D3_ABAE_94F400C10000__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+/////////////////////////////////////////////////////////////////////////////
+// CShortcutListBoxView view
+
+class CShortcutListBoxView : public CView
+{
+protected:
+	CShortcutListBoxView();           // protected constructor used by dynamic creation
+	DECLARE_DYNCREATE(CShortcutListBoxView)
+
+// Attributes
+public:
+	CXTPShortcutListBox m_wndShortcutListBox;
+	CXTPPageScroller    m_wndPageScroller;
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CShortcutListBoxView)
+	protected:
+	virtual void OnDraw(CDC* pDC);      // overridden to draw this view
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	virtual ~CShortcutListBoxView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CShortcutListBoxView)
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg LRESULT OnShortcutLBoxNotify(WPARAM lParam, LPARAM wParam);
+	//}}AFX_MSG
+	afx_msg void OnShortcutListScroll(NMHDR* pNMHDR, LRESULT * pResult);
+	afx_msg void OnShortcutListCalcSize(NMHDR* pNMHDR, LRESULT* pResult);
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_SHORTCUTLISTBOXVIEW_H__790EB214_2913_11D3_ABAE_94F400C10000__INCLUDED_)
