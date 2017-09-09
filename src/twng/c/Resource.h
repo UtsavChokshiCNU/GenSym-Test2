6deb2082cbc45d498211d856a87e1c7944d0d5a8
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:32:48 +0400

    Commiting all

---
+++ src/twng/c/Resource.h
@@ -0,0 +1,125 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Twng.rc
+//
+
+/* Link statically with the XTP library. Also must be defined in stdafx.h */
+#define _XTP_STATICLINK
+/* Added for resources used by CXTOutBarCtrl -fmw, 3/8/05 */
+#define _XTP_INCLUDE_CONTROLS
+
+#define IDR_MANIFEST                    1
+#define IDR_MAINFRAME                   128
+#define IDR_TwngTYPE                    129
+#define IDR_EDITOR                      130
+#define IDR_OUTPUT_PANE                 131
+#define IDB_FILETREE                    133
+#define IDD_DYNAMIC_BASE_DLG            134
+#define IDD_TABS_DLG                    135
+#define IDR_PRINTDIALOG                 146
+#define IDD_PROPDLG                     147
+#define IDD_MODALEDITOR                 148
+#define IDD_CONFIRMEXIT                 149     // id for confirm exit dialog
+#define IDD_DEBUGGER 			1235
+#define IDD_EDITOR 			1236
+
+#define IDC_EDITID                      1237
+#define IDC_EDITVAL                     1238
+
+#define IDR_FILE						1239
+#define IDR_EDIT						1240
+#define IDR_VIEW						1241
+#define IDR_RUN							1242
+#define IDR_TOOLS						1243
+#define IDR_WINDOW						1244
+#define IDR_WORKSPACE					1245
+
+#define ID_PRINTME                      32776
+
+#define IDPAUSE 204
+#define ID_EDIT_SAVE_AND_EXIT 0xC003 // This ID has an icon, which shows up in G2 menus!
+#define ID_TOGGLE_TOOLBAR 206
+#define ID_EDIT_HELP 207
+#define ID_GOTO_ITEM 0xC004	     // This ID has an icon, which shows up in G2 menus!
+
+/* Copied from ext/c/msincls.h */
+#define TWPRO_CONNECT                   1001 /* Keep these in sync with commands0.lisp */
+#define TWPRO_CLOSEALL                  1002
+#define TWPRO_CLOSE                     1003
+#define TWPRO_CASCADE                   1005
+#define TWPRO_EXIT                      1006
+#define TWPRO_TILE                      1007
+#define TWPRO_TILE_VERTICALLY           1008
+#define TWPRO_TILE_HORIZONTALLY         1009
+#define TWPRO_ABOUT                     1010
+#define TWPRO_TEST                      1011
+#define TWPRO_STATUSBAR                 1012
+#define TWPRO_MORE_WINDOWS              65279 //Gensym-2041-TAM3-CH-SUCA-JIRA-46986-Add the ability to exclude "more windows" menu choice
+//#define TWPRO_OPEN                      1013
+//#define TWPRO_SAVE_AS                   1014
+#define TWPRO_TOOLBARS                  1015
+#define TWPRO_INSERT_OBJECT             1016 /* NYI */
+#define TWPRO_HELP                      1017
+#define TWPRO_MENU_SMALL            	1020  //HQ-5476610 menu font size options for Twng - Nov 19 2008
+#define TWPRO_MENU_MEDIUM             	1021  //HQ-5476610 menu font size options for Twng - Nov 19 2008
+#define TWPRO_MENU_LARGE              	1022  //HQ-5476610 menu font size options for Twng - Nov 19 2008
+#define TWPRO_PRINTWINDOW               1018
+#define IDC_DONTASK_CHECK               1019     // id for “Don’t ask again” checkbox
+
+#define TWPRO_LOAD_KB					1100
+#define TWPRO_MERGE_KB					1101
+#define TWPRO_SAVE_KB					1102
+#define TWPRO_PRINTER_SETUP				1103
+#define TWPRO_PRINT_TO_SERVER			1104
+#define TWPRO_PRINT						1105
+#define TWPRO_LOG_OUT					1106
+#define TWPRO_SHUT_DOWN_G2				1107
+#define TWPRO_DISCONNECT				1108
+
+#define TWPRO_DELETE					1109
+
+#define TWPRO_FULL_SCALE				1110
+#define TWPRO_SCALE_TO_FIT				1111
+#define TWPRO_ZOOM_IN					1112
+#define TWPRO_ZOOM_OUT					1113
+#define TWPRO_QUARTER_SCALE				1114
+#define TWPRO_FOUR_TIMES_TO_SCALE		1115
+#define TWPRO_TOGGLE_VISIBLE_GRID		1116
+#define TWPRO_REFRESH					1117
+
+#define TWPRO_START						1118
+#define TWPRO_PAUSE						1119
+#define TWPRO_RESUME					1120
+#define TWPRO_CONTINUE					1121
+#define TWPRO_RESET						1122
+#define TWPRO_RESTART					1123
+#define TWPRO_RUN_OPTIONS				1124
+
+#define TWPRO_INSPECT					1125
+#define TWPRO_NEATLY_STACK_WINDOWS		1126
+#define TWPRO_NETWORK_INFO				1127
+
+#define TWPRO_NEW_WORKSPACE				1128
+#define TWPRO_GET_WORKSPACE				1129
+#define TWPRO_NEW_OBJECT				1130
+#define TWPRO_NEW_RULE					1131
+#define TWPRO_NEW_DISPLAY				1132
+#define TWPRO_NEW_DEFINITION			1133
+#define TWPRO_NEW_FREE_TEXT				1134
+#define TWPRO_NEW_BUTTON				1135
+#define TWPRO_TABLE						1136
+#define TWPRO_COLOR						1137
+#define TWPRO_MOVE						1138
+#define TWPRO_LIFT_TO_TOP				1139
+#define TWPRO_DROP_TO_BOTTOM			1140
+#define TWPRO_SHRINK_WRAP				1141
+#define TWPRO_DELETE_WORKSPACE			1142
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        148
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
