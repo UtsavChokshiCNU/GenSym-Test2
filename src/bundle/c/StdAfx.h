dd31fa8663d70b6c18c92d8c5bbf52026214ce5a
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:43:23 +0400

    Commiting all

---
+++ src/bundle/c/StdAfx.h
@@ -0,0 +1,30 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__697EAF33_578F_11D4_9B48_B04A30000000__INCLUDED_)
+#define AFX_STDAFX_H__697EAF33_578F_11D4_9B48_B04A30000000__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// TODO: reference additional headers your program requires here
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <shlwapi.h>
+
+#include "codes.h"
+#include "family-descriptor.h"
+#include "tag-value-parser.h"
+
+
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__697EAF33_578F_11D4_9B48_B04A30000000__INCLUDED_)
