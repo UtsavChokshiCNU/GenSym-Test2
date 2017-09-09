a9e4312e7c31374e4a95259b1b261499f9ed47e0
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:49:34 +0400

    Commiting all

---
+++ src/xtp/Samples/SyntaxEdit/MDITextEditor/ColorsDataEntry.h
@@ -0,0 +1,37 @@
+
+	sb += _T("; parent schema\r\n");
+	sb += _T("; SchemaName=Default\r\n");
+	sb += _T("[c_CPP]\r\n");
+	sb += _T("colorBK=0xFFFFFF\r\n");
+	sb += _T("colorFG=0x000000\r\n");
+	sb += _T("\r\n");
+	sb += _T("[c_URL]\r\n");
+	sb += _T("colorFG=0x0000FF\r\n");
+	sb += _T("italic=1\r\n");
+	sb += _T("\r\n");
+	sb += _T("[c_CPPCommentML]\r\n");
+	sb += _T("colorFG=0x008200\r\n");
+	sb += _T("\r\n");
+	sb += _T("[c_CPPCommentSL]\r\n");
+	sb += _T("colorFG=0x008200\r\n");
+	sb += _T("\r\n");
+	sb += _T("[c_Token1]\r\n");
+	sb += _T("colorFG=0x0011AA\r\n");
+	sb += _T("\r\n");
+	//----------------------------------------
+	sb += _T("SchemaName=Alternative\r\n");
+	sb += _T("[Main]\r\n");
+	sb += _T("ParentSchema=Default\r\n");
+	sb += _T("\r\n");
+	sb += _T("[c_URL]\r\n");
+	sb += _T("colorBK=0xCCFF00\r\n");
+	sb += _T("colorFG=0xAA1100\r\n");
+	sb += _T("Bold =1\r\n");
+	sb += _T("\r\n");
+	sb += _T("[c_CPPCommentSL]\r\n");
+	sb += _T("colorFG=0xCCFF00\r\n");
+	sb += _T("colorBK=0xAA1100\r\n");
+	sb += _T("Italic=1 \r\n");
+	sb += _T("\r\n");
+	//----------------------------------------
+	sColorTheme = sb;
