d2fe3ce91d88fb8b14336d66c95ab00ca1a67227
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:47:53 +0400

    Commiting all

---
+++ src/xtp/Samples/Chart/StockSample/MainFrm.h
@@ -0,0 +1,73 @@
+// MainFrm.h : interface of the CMainFrame class
+//
+// --------------------------------------------------------------------------
+// COPYRIGHT NOTICE:
+// --------------------------------------------------------------------------
+// 
+// "Codejock Chart" is a MFC extension library for creating modern charting 
+// applications. This file and the code herein are part of the Codejock Chart
+// MFC extension library.
+//
+// This code is protected under U.S. Copyright Law and International treaties
+// and is intended for registered and licensed use only. Any other use is 
+// strictly prohibited. This code is subject to the terms and conditions 
+// outlined in the "Codejock Chart" End User License Agreement (EULA).
+//
+// Copyright (c) 1998-2011 Codejock Technologies LLC, All Rights Reserved.
+// http://www.codejock.com
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_MAINFRM_H__A6632B23_CEAB_48C8_9ADC_F1272F782393__INCLUDED_)
+#define AFX_MAINFRM_H__A6632B23_CEAB_48C8_9ADC_F1272F782393__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CMainFrame : public CFrameWnd
+{
+	
+protected: // create from serialization only
+	CMainFrame();
+	DECLARE_DYNCREATE(CMainFrame)
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CMainFrame)
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CMainFrame();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:  // control bar embedded members
+	CStatusBar  m_wndStatusBar;
+	CToolBar    m_wndToolBar;
+
+// Generated message map functions
+protected:
+	//{{AFX_MSG(CMainFrame)
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnGetMinMaxInfo(MINMAXINFO* lpMMI);
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_MAINFRM_H__A6632B23_CEAB_48C8_9ADC_F1272F782393__INCLUDED_)
