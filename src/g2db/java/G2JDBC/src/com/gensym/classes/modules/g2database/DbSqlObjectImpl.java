3f70fe97ed58a7e5c46c45ebeb0ea66979cb7f42
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:04:15 +0400

    Commiting all

---
+++ src/g2db/java/G2JDBC/src/com/gensym/classes/modules/g2database/DbSqlObjectImpl.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (C) 1986-2017 Gensym Corporation.
+ *  All Rights Reserved.
+ *
+ *      DbSqlObjectImpl.java
+ *
+ *   Description: Generated Interface file. Do not edit!
+ *
+ *        Author: Gensym Corp.
+ *
+ *       Version: 5.1 Rev. 1
+ *
+ *          Date: Thu Jun 18 17:47:13 EEST 2009
+ *
+ */
+
+
+package com.gensym.classes.modules.g2database;
+
+import com.gensym.classes.*;
+import com.gensym.util.Structure;
+import com.gensym.util.Sequence;
+import com.gensym.util.Symbol;
+import com.gensym.util.symbol.SystemAttributeSymbols;
+import com.gensym.jgi.*;
+import com.gensym.classes.Object;
+
+public class DbSqlObjectImpl extends com.gensym.classes.modules.g2database.DbConfigObjectImpl implements DbSqlObject {
+
+
+  static final long serialVersionUID = 2L;
+
+  /* Generated constructors */
+
+  public DbSqlObjectImpl() {
+    super();
+  }
+
+  public DbSqlObjectImpl(G2Access context, int handle, Structure attributes) {
+    super (context, handle, attributes);
+  }
+
+
+  public DbSqlObjectImpl(G2Access context, int handle, int handleVersion, G2__BaseImpl wrap, Structure attributes) {
+    super (context, handle, handleVersion, wrap, attributes);
+  }
+
+
+  private static String NoBodyExceptionString = "This method has no implementation for local access";
+  
+  
+}
