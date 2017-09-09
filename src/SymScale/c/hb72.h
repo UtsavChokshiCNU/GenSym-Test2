47b8b82e933169e87a0672707dc511b88eacb441
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 15:39:30 +0400

    Commit-1

---
+++ src/SymScale/c/hb72.h
@@ -0,0 +1,33 @@
+/* hb72.h -- Header File for hb72.c
+ * Copyright (C) 1986-2017 Gensym Corporation.  All Rights Reserved.
+ */
+
+
+/* Symbol Definitions */
+
+extern Object Pclos;
+extern Object Pclos_run;
+extern Object Pclosx;
+extern Object Pcl;
+extern Object Pcl_user;
+extern Object Pkeyword;
+extern Object Ptrun;
+extern Object Ptx;
+
+/* function declarations */
+
+#ifdef USE_PROTOTYPES
+
+extern Object add_character_description_1(int,...);
+extern Object define_ab_font(int,...);
+extern Object regenerate_optimized_constant(Object);
+
+#else
+
+extern Object add_character_description_1();
+extern Object define_ab_font();
+extern Object regenerate_optimized_constant();
+
+#endif
+
+/* variables/constants */
