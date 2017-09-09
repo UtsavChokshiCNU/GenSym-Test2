9460233289e019209f46445ef2ffb030ab0ce61a
Utsav Chokshi <utsavchokshi@Utsavs-MacBook-Pro.local>
2017-09-09 16:05:04 +0400

    Commiting all

---
+++ src/g2i/java/com/gensym/G2SNMP/TaskInitException.java
@@ -0,0 +1,12 @@
+package com.gensym.G2SNMP;
+
+public class TaskInitException extends Exception {
+
+    public TaskInitException() {
+    }
+
+    public TaskInitException(String s) {
+        super(s);
+    }
+
+}
